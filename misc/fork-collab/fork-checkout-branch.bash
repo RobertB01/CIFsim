317bf9840144b7aee7e59329e80f35f08295ab68
Dennis Hendriks <dh_tue@hotmail.com>
2021-12-07 16:19:00 +0100

    #184 Explain easier collaboration with forks/contributors.
     - Simplified information on collaboration documentation page.
     - Moved more extensive information to development process page.
     - Added some scripts to make collaborating easier.

---
+++ misc/fork-collab/fork-checkout-branch.bash
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+################################################################################
+# Copyright (c) 2021 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the terms
+# of the MIT License which is available at https://opensource.org/licenses/MIT
+#
+# SPDX-License-Identifier: MIT
+################################################################################# 
+
+# This script is only meant for checkout of branches on the Eclipse Foundation
+# GitLab server (gitlab.eclipse.org) that are forks of the official Eclipse
+# ESCET Git repository.
+
+# Usage: misc/fork-collab/fork-checkout-branch.bash <username> <branch_name>
+
+set -e -u
+
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+cd $SCRIPTPATH
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 <username> <branch_name>"
+    exit 1
+fi
+
+GITLAB_USERNAME=$1
+GITLAB_BRANCH_NAME=$2
+GITLAB_URL=https://gitlab.eclipse.org/$GITLAB_USERNAME/escet.git
+LOCAL_BRANCH_NAME=$GITLAB_USERNAME/escet/$GITLAB_BRANCH_NAME
+
+echo "Username:                  $GITLAB_USERNAME"
+echo "GitLab remote branch name: $GITLAB_BRANCH_NAME"
+echo "GitLab URL:                $GITLAB_URL"
+echo "Local branch name:         $LOCAL_BRANCH_NAME"
+
+git fetch "$GITLAB_URL" "$GITLAB_BRANCH_NAME"
+git checkout -b "$LOCAL_BRANCH_NAME" FETCH_HEAD
