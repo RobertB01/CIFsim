f2504004bbe536d0e35956961cec0f41e3b1fa7e
Ferdie Reijnen <ferdie.reijnen@gmail.com>
2022-01-12 22:01:56 +0100

    #142 Added arguments to PlcNumberBits enumeration.

--- cif/org.eclipse.escet.cif.cif2plc/src/org/eclipse/escet/cif/cif2plc/options/PlcNumberBitsOption.java
+++ cif/org.eclipse.escet.cif.cif2plc/src/org/eclipse/escet/cif/cif2plc/options/PlcNumberBitsOption.java
@@ -22,54 +22,31 @@ public class PlcNumberBitsOption extends EnumOption<PlcNumberBits> {
     /** Constructor for the {@link PlcNumberBitsOption} class. */
     public PlcNumberBitsOption() {
         super("PLC number bits",
-                "BITS is the maximum number of bits supported by the PLC for representing numeric values. "
-                        + "Allowed values are \"auto\", \"32\" and\"64\". [DEFAULT=auto]",
+                "BITS is the maximum number of bits supported by the PLC for representing numeric values. Specifiy "
+                        + "\"auto\" to automatically determine the maximum number of bits, \"32\" for 32-bits, or "
+                        + "\"64\" for 64-bits. [DEFAULT=auto]",
                 'b', "number-bits", "BITS", PlcNumberBits.AUTO, true,
                 "The maximum number of bits supported by the PLC for representing numeric values.");
     }
 
     @Override
     protected String getDialogText(PlcNumberBits value) {
-        switch (value) {
-            case AUTO:
-                return "Automatic";
-            case BITS_32:
-                return "32-bit";
-            case BITS_64:
-                return "64-bit";
-            default:
-                throw new RuntimeException("Unknown number of bits: " + value);
-        }
+        return value.dialogText;
     }
 
     @Override
     public PlcNumberBits parseValue(String optName, String value) {
-        if (value.equals("auto")) {
-            return PlcNumberBits.AUTO;
-        } else if (value.equals("32")) {
-            return PlcNumberBits.BITS_32;
-        } else if (value.equals("64")) {
-            return PlcNumberBits.BITS_64;
+        for (PlcNumberBits option: PlcNumberBits.values()) {
+            if (value.equals(option.cmdValueTxt)) {
+                return option;
+            }
         }
-        throw new InvalidOptionException("Unknown option value.");
+        throw new InvalidOptionException("Unknown option value: " + value);
     }
 
     @Override
     public String[] getCmdLine(Object value) {
-        String valueTxt;
-        switch ((PlcNumberBits)value) {
-            case AUTO:
-                valueTxt = "auto";
-                break;
-            case BITS_32:
-                valueTxt = "32";
-                break;
-            case BITS_64:
-                valueTxt = "64";
-                break;
-            default:
-                throw new RuntimeException("Unknown PLC number bits value.");
-        }
+        String valueTxt = ((PlcNumberBits)value).cmdValueTxt;
         return new String[] {"--" + cmdLong + "=" + valueTxt};
     }
 
