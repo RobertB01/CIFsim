1cb0a21fc82c029290805b5326bc825a3f45c1be
Albert Hofkamp <a.t.hofkamp@tue.nl>
2022-01-19 13:58:26 +0100

    #293 Rewrite choice rail layout.

--- common/org.eclipse.escet.common.raildiagrams/src/org/eclipse/escet/common/raildiagrams/railroad/ChoiceNode.java
+++ common/org.eclipse.escet.common.raildiagrams/src/org/eclipse/escet/common/raildiagrams/railroad/ChoiceNode.java
@@ -37,24 +37,24 @@ import org.eclipse.escet.common.raildiagrams.util.DebugDisplayKind;
 /** Node that builds a diagram to pick one of the child nodes. */
 public class ChoiceNode extends DiagramElement {
     /** Alternatives of the choice. */
-    public final List<DiagramElement> alts;
+    public final List<DiagramElement> alternatives;
 
-    /** Proxies of the {@link #alts} elements. */
+    /** Proxies of the {@link #alternatives} elements. */
     private List<ProxyDiagramElement> altProxies;
 
     /**
      * Choice between alternatives.
      *
-     * @param alts Alternatives to choose from.
+     * @param alternatives Alternatives to choose from.
      * @param id Identifying number of the diagram element.
      */
-    public ChoiceNode(List<DiagramElement> alts, int id) {
+    public ChoiceNode(List<DiagramElement> alternatives, int id) {
         super("choice", id);
-        this.alts = alts;
-        Assert.check(alts.size() > 1);
+        this.alternatives = alternatives;
+        Assert.check(alternatives.size() > 1);
     }
 
-    @SuppressWarnings("null") // False positives in the 'else', since the 'if' case initializes them first.
+    @SuppressWarnings("null") // False positives in accessing topElement and lastElement.
     @Override
     public void create(Configuration config, int direction) {
         double railWidth = config.getRailWidth();
@@ -62,105 +62,129 @@ public class ChoiceNode extends DiagramElement {
         double arcSize = config.getRealValue("choice.arc-radius");
         double choiceVertPadding = config.getRealValue("choice.padding.vertical");
 
-        altProxies = listc(alts.size());
-        ProxyDiagramElement topElement = null;
-        TopRightArc leftArcDown = null;
-        TopLeftArc rightArcDown = null;
+        // Arc at the left down to the alternatives.
+        TopRightArc leftArcDown = new TopRightArc(solver, "leftarc-down", railColor, arcSize, railWidth);
+        addGraphic(leftArcDown);
+        solver.addEq(leftArcDown.left, 0, left);
+        solver.addEq(leftArcDown.top, 0, connectTop);
 
+        // Vertical line down at the left. Connect arcs to the alternatives horizontally only except for
+        // the bottom alternative.
+        VertLine leftVer = new VertLine(solver, "left-vert", railColor, railWidth);
+        addGraphic(leftVer);
+        leftArcDown.connectLine(solver, leftVer);
+
+        // Arc up from the alternatives to the horizontal exit.
+        TopLeftArc rightArcDown = new TopLeftArc(solver, "rightarc-up", railColor, arcSize, railWidth);
+        addGraphic(rightArcDown);
+        solver.addEq(rightArcDown.right, 0, right);
+        solver.addEq(rightArcDown.top, 0, connectTop);
+
+        // Vertical line up at the right. Connect arcs from the alternatives horizontally only except for
+        // the bottom alternative.
+        VertLine rightVer = new VertLine(solver, "right-vert", railColor, railWidth);
+        addGraphic(rightVer);
+        rightArcDown.connectLine(solver, rightVer);
+
+        // Construct alternatives.
         int i = 0;
-        ProxyDiagramElement prevElement = null;
-        for (DiagramElement alt: alts) {
+        ProxyDiagramElement topElement = null;
+        ProxyDiagramElement lastElement = null;
+        BottomLeftArc lastLeftArc = null;
+        BottomRightArc lastRightArc = null;
+        altProxies = listc(alternatives.size());
+        for (DiagramElement alt: alternatives) {
             idbg();
             alt.create(config, direction);
             ddbg();
             ProxyDiagramElement altProxy = addDiagramElement(alt, fmt("alt-%d", i));
             altProxies.add(altProxy);
 
-            // All alternatives must be within the choice node.
-            solver.addLe(top, 0, altProxy.top);
-            solver.addLe(altProxy.bottom, 0, bottom);
+            if (topElement == null) {
+                topElement = altProxy; // First element.
+                lastElement = altProxy; // and also currently last element.
 
-            if (prevElement == null) {
-                topElement = altProxy;
-
-                // First element, add arcs going to the other alternatives.
-                solver.addEq(connectTop, 0, altProxy.connectTop);
-
-                double minHorWidth = arcSize * 2 - railWidth; // Minimal length of the entry and exit lines.
-                HorLine leftHor = new HorLine(solver, "left-hor", railColor, railWidth);
+                // Connect proxie directly to the choice entry/exits.
+                HorLine leftHor = new HorLine(solver, "left-alt0", railColor, railWidth);
                 addGraphic(leftHor);
+                solver.addEq(connectTop, 0, leftHor.top);
                 solver.addEq(left, 0, leftHor.left);
-                solver.addEq(leftHor.right, 0, altProxy.left);
-                solver.addLe(leftHor.left, minHorWidth, leftHor.right);
-                solver.addEq(leftHor.top, 0, connectTop);
+                solver.addEq(leftHor.right, 1, altProxy.left);
+                solver.addEq(leftHor.top, 0, altProxy.connectTop);
+                solver.addLe(leftHor.left, arcSize, leftHor.right); // Make line slightly longer than arc.
 
-                HorLine rightHor = new HorLine(solver, "right-hor", railColor, railWidth);
+                HorLine rightHor = new HorLine(solver, "right-alt0", railColor, railWidth);
                 addGraphic(rightHor);
-                solver.addEq(rightHor.left, 0, altProxy.right);
+                solver.addEq(rightHor.left, 1, altProxy.right);
+                solver.addEq(rightHor.top, 0, connectTop);
                 solver.addEq(rightHor.right, 0, right);
-                solver.addLe(rightHor.left, minHorWidth, rightHor.right);
-                solver.addEq(connectTop, 0, rightHor.top);
+                solver.addEq(rightHor.top, 0, altProxy.connectTop);
+                solver.addLe(rightHor.left, arcSize, rightHor.right); // Make line slightly longer than arc.
 
-                leftArcDown = new TopRightArc(solver, "leftarc-down", railColor, arcSize, railWidth);
-                addGraphic(leftArcDown);
-                solver.addEq(leftArcDown.left, 0, left);
-                solver.addEq(leftArcDown.top, 0, connectTop);
-
-                rightArcDown = new TopLeftArc(solver, "rightarc-down", railColor, arcSize, railWidth);
-                addGraphic(rightArcDown);
-                solver.addEq(rightArcDown.right, 0, right);
-                solver.addEq(rightArcDown.top, 0, connectTop);
             } else {
-                solver.addLe(prevElement.bottom, choiceVertPadding, altProxy.top); // Min distance to previous choice.
+                // Not first element.
 
-                VertLine leftVer = new VertLine(solver, fmt("left-ver-%d", i), railColor, railWidth);
-                addGraphic(leftVer);
-                leftArcDown.connectLine(solver, leftVer);
-                BottomLeftArc leftConnect = new BottomLeftArc(solver, fmt("left-connect-%d", i), railColor, arcSize,
-                        railWidth);
-                addGraphic(leftConnect);
-                leftConnect.connectLine(solver, leftVer);
+                // Keep distance from predecessor.
+                solver.addLe(lastElement.bottom, choiceVertPadding, altProxy.top);
+                lastElement = altProxy; // New last element.
 
-                VertLine rightVer = new VertLine(solver, fmt("right-ver-%d", i), railColor, railWidth);
-                addGraphic(rightVer);
-                rightArcDown.connectLine(solver, rightVer);
-                BottomRightArc rightConnect = new BottomRightArc(solver, fmt("right-connect-%d", i), railColor, arcSize,
-                        railWidth);
-                addGraphic(rightConnect);
-                rightConnect.connectLine(solver, rightVer);
+                // If 2nd alternative, connect vertically with topElement to align the latter as well.
+                if (i == 1) {
+                    if (direction > 0) {
+                        solver.addEq(topElement.left, 0, altProxy.left);
+                    } else {
+                        solver.addEq(topElement.right, 0, altProxy.right);
+                    }
+                }
 
-                HorLine hLine = new HorLine(solver, fmt("alt-hor-%d", i), railColor, railWidth);
+                // Construct arcs to connect to and attach them horizontally to the vertical lines at either side.
+                lastLeftArc = new BottomLeftArc(solver, fmt("left-arc-%d", i), railColor, arcSize, railWidth);
+                addGraphic(lastLeftArc);
+                solver.addEq(lastLeftArc.left, 0, leftVer.left);
+
+                lastRightArc = new BottomRightArc(solver, fmt("right-arc-%d", i), railColor, arcSize, railWidth);
+                addGraphic(lastRightArc);
+                solver.addEq(lastRightArc.right, 0, rightVer.right);
+
+                // Connect the alternatives to the arcs.
+                HorLine hLine = new HorLine(solver, fmt("alt-trail-%d", i), railColor, railWidth);
                 addGraphic(hLine);
                 if (direction > 0) {
-                    altProxy.connectLeft(solver, leftConnect, railWidth);
+                    altProxy.connectLeft(solver, lastLeftArc, railWidth);
                     altProxy.connectRight(solver, hLine);
-                    rightConnect.connectLine(solver, hLine);
-
-                    solver.addEq(topElement.left, 0, altProxy.left); // Align all alts at their left side.
+                    lastRightArc.connectLine(solver, hLine);
                 } else {
-                    leftConnect.connectLine(solver, hLine);
+                    lastLeftArc.connectLine(solver, hLine);
                     altProxy.connectLeft(solver, hLine);
-                    altProxy.connectRight(solver, rightConnect, railWidth);
-
-                    solver.addEq(topElement.right, 0, altProxy.right); // Align all alts at their right side.
+                    altProxy.connectRight(solver, lastRightArc, railWidth);
                 }
             }
-            prevElement = altProxy;
             i++;
         }
 
-        solver.solve("choice", config);
+        // Connect first and last element to the choice node top and bottom.
+        solver.addLe(top, 0, topElement.top);
+        solver.addLe(lastElement.bottom, 0, bottom);
+
+        // Connect last arcs to the vertical line vertically, to force the lines to the correct lengths.
+        solver.addEq(leftVer.bottom, 1, lastLeftArc.top);
+        solver.addEq(rightVer.bottom, 1, lastRightArc.top);
 
         boolean dumpEquations = config.getDebugSetting(DebugDisplayKind.EQUATIONS);
-        boolean dumpRelCoords = config.getDebugSetting(DebugDisplayKind.REL_COORDINATES);
-        if ((dumpEquations || dumpRelCoords) && dodbg()) {
-            writeDumpHeaderElements(this, alts);
+        if (dumpEquations && dodbg()) {
+            writeDumpHeaderElements(this, alternatives);
             dbg();
 
-            if (dumpEquations) {
-                solver.dumpRelations();
-                dbg();
-            }
+            solver.dumpRelations();
+            dbg();
+        }
+        solver.solve("choice", config);
+
+        boolean dumpRelCoords = config.getDebugSetting(DebugDisplayKind.REL_COORDINATES);
+        if (dumpRelCoords && dodbg()) {
+            writeDumpHeaderElements(this, alternatives);
+            dbg();
+
             if (dumpRelCoords) {
                 dumpElementBox();
                 dbg();
