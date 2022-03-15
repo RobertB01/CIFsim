fe32e32ffb9319ccb071dc109b9a543fb72fbe26
Albert Hofkamp <a.t.hofkamp@tue.nl>
2022-03-15 15:30:45 +0100

    #330 Streamify collect utils.

--- cif/org.eclipse.escet.cif.common/src/org/eclipse/escet/cif/common/CifCollectUtils.java
+++ cif/org.eclipse.escet.cif.common/src/org/eclipse/escet/cif/common/CifCollectUtils.java
@@ -13,12 +13,17 @@
 
 package org.eclipse.escet.cif.common;
 
+import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Deque;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.eclipse.escet.cif.metamodel.cif.ComplexComponent;
 import org.eclipse.escet.cif.metamodel.cif.Component;
 import org.eclipse.escet.cif.metamodel.cif.ComponentDef;
-import org.eclipse.escet.cif.metamodel.cif.ComponentInst;
 import org.eclipse.escet.cif.metamodel.cif.Group;
 import org.eclipse.escet.cif.metamodel.cif.IoDecl;
 import org.eclipse.escet.cif.metamodel.cif.automata.Automaton;
@@ -50,19 +55,12 @@ public class CifCollectUtils {
      * @param events The events collected so far. Is modified in-place.
      */
     public static void collectEvents(ComplexComponent comp, Collection<Event> events) {
-        // Collect locally.
-        for (Declaration decl: comp.getDeclarations()) {
-            if (decl instanceof Event) {
-                events.add((Event)decl);
-            }
-        }
-
-        // Collect recursively.
-        if (comp instanceof Group) {
-            for (Component child: ((Group)comp).getComponents()) {
-                collectEvents((ComplexComponent)child, events);
-            }
-        }
+        getComplexComponents(comp)
+                .forEach(cc ->
+                {
+                    cc.getDeclarations().stream().filter(decl -> decl instanceof Event)
+                            .forEach(decl -> events.add((Event)decl));
+                });
     }
 
     /**
@@ -76,19 +74,11 @@ public class CifCollectUtils {
      * @param ctrlEvents The controllable events collected so far. Is modified in-place.
      */
     public static void collectControllableEvents(ComplexComponent comp, Collection<Event> ctrlEvents) {
-        // Collect locally.
-        for (Declaration decl: comp.getDeclarations()) {
-            if (decl instanceof Event && ((Event)decl).getControllable() != null && ((Event)decl).getControllable()) {
-                ctrlEvents.add((Event)decl);
-            }
-        }
-
-        // Collect recursively.
-        if (comp instanceof Group) {
-            for (Component child: ((Group)comp).getComponents()) {
-                collectControllableEvents((ComplexComponent)child, ctrlEvents);
-            }
-        }
+        getComplexComponents(comp).forEach(cc -> {
+            cc.getDeclarations().stream().filter(decl -> decl instanceof Event
+                    && ((Event)decl).getControllable() != null && ((Event)decl).getControllable())
+                    .forEach(decl -> ctrlEvents.add((Event)decl));
+        });
     }
 
     /**
@@ -102,15 +92,7 @@ public class CifCollectUtils {
      * @param automata The automata collected so far. Is modified in-place.
      */
     public static void collectAutomata(ComplexComponent comp, Collection<Automaton> automata) {
-        if (comp instanceof Automaton) {
-            // Add automaton.
-            automata.add((Automaton)comp);
-        } else {
-            // Collect recursively.
-            for (Component child: ((Group)comp).getComponents()) {
-                collectAutomata((ComplexComponent)child, automata);
-            }
-        }
+        getComplexComponents(comp).filter(cc -> cc instanceof Automaton).forEach(cc -> automata.add((Automaton)cc));
     }
 
     /**
@@ -124,19 +106,10 @@ public class CifCollectUtils {
      * @param variables The discrete and input variables collected so far. Is modified in-place.
      */
     public static void collectDiscAndInputVariables(ComplexComponent comp, Collection<Declaration> variables) {
-        // Collect locally.
-        for (Declaration decl: comp.getDeclarations()) {
-            if (decl instanceof DiscVariable || decl instanceof InputVariable) {
-                variables.add(decl);
-            }
-        }
-
-        // Collect recursively.
-        if (comp instanceof Group) {
-            for (Component child: ((Group)comp).getComponents()) {
-                collectDiscAndInputVariables((ComplexComponent)child, variables);
-            }
-        }
+        getComplexComponents(comp).forEach(cc -> {
+            cc.getDeclarations().stream().filter(decl -> decl instanceof DiscVariable || decl instanceof InputVariable)
+                    .forEach(decl -> variables.add(decl));
+        });
     }
 
     /**
@@ -150,15 +123,7 @@ public class CifCollectUtils {
      * @param declarations The declarations collected so far. Is modified in-place.
      */
     public static void collectDeclarations(ComplexComponent comp, Collection<Declaration> declarations) {
-        // Collect locally.
-        declarations.addAll(comp.getDeclarations());
-
-        // Collect recursively.
-        if (comp instanceof Group) {
-            for (Component child: ((Group)comp).getComponents()) {
-                collectDeclarations((ComplexComponent)child, declarations);
-            }
-        }
+        getComplexComponents(comp).forEach(cc -> declarations.addAll(cc.getDeclarations()));
     }
 
     /**
@@ -172,15 +137,7 @@ public class CifCollectUtils {
      * @param declarations The I/O declarations collected so far. Is modified in-place.
      */
     public static void collectIoDeclarations(ComplexComponent comp, Collection<IoDecl> declarations) {
-        // Collect locally.
-        declarations.addAll(comp.getIoDecls());
-
-        // Collect recursively.
-        if (comp instanceof Group) {
-            for (Component child: ((Group)comp).getComponents()) {
-                collectIoDeclarations((ComplexComponent)child, declarations);
-            }
-        }
+        getComplexComponents(comp).forEach(cc -> declarations.addAll(cc.getIoDecls()));
     }
 
     /**
@@ -190,23 +147,107 @@ public class CifCollectUtils {
      * @param enumDecls The enumeration declarations collected so far. Is modified in-place.
      */
     public static void collectEnumDecls(ComplexComponent comp, Collection<EnumDecl> enumDecls) {
-        // Collect locally.
-        for (Declaration decl: comp.getDeclarations()) {
-            if (decl instanceof EnumDecl) {
-                enumDecls.add((EnumDecl)decl);
+        getComplexComponents(comp, true).forEach(cc -> {
+            cc.getDeclarations().stream().filter(decl -> decl instanceof EnumDecl)
+                    .forEach(decl -> enumDecls.add((EnumDecl)decl));
+        });
+    }
+
+    /** Class to construct a finite stream of {@link ComplexComponent} in the given root component. */
+    private static class ComplexComponentSpliterator implements Spliterator<ComplexComponent> {
+        /** Whether to search component definitions as well. */
+        private final boolean traverseCompDefs;
+
+        /** Queue with discovered complex components that have not been processed yet. */
+        private Deque<ComplexComponent> notDone = new ArrayDeque<>();
+
+        /**
+         * Constructor of the {@link ComplexComponentSpliterator} class.
+         *
+         * @param traverseCompDefs Whether to search component definitions as well.
+         * @param comp Root component to traverse for all its {@link ComplexComponent}s.
+         */
+        public ComplexComponentSpliterator(ComplexComponent comp, boolean traverseCompDefs) {
+            this.traverseCompDefs = traverseCompDefs;
+            notDone.add(comp);
+        }
+
+        @Override
+        public int characteristics() {
+            return DISTINCT | IMMUTABLE | NONNULL | ORDERED;
+        }
+
+        @Override
+        public long estimateSize() {
+            long estimate = 0;
+            for (ComplexComponent comp: notDone) {
+                if (comp instanceof Group) {
+                    Group grp = (Group)comp;
+                    estimate += grp.getComponents().size();
+                } else {
+                    estimate++;
+                }
+            }
+            return estimate; // Under-estimated size.
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super ComplexComponent> action) {
+            if (notDone.isEmpty()) {
+                return false;
+            }
+
+            ComplexComponent comp = notDone.poll();
+            expandGroup(comp);
+            action.accept(comp);
+            return true;
+        }
+
+        /**
+         * Examine the given complex component for child complex components.
+         *
+         * @param comp Component to examine.
+         */
+        private void expandGroup(ComplexComponent comp) {
+            if (comp instanceof Group) {
+                Group grp = (Group)comp;
+                for (Component child: grp.getComponents()) {
+                    if (child instanceof ComplexComponent) {
+                        notDone.add((ComplexComponent)child);
+                    }
+                }
+                if (traverseCompDefs) {
+                    for (ComponentDef compDef: grp.getDefinitions()) {
+                        notDone.add(compDef.getBody());
+                    }
+                }
             }
         }
 
-        // Collect recursively.
-        if (comp instanceof Group) {
-            for (Component child: ((Group)comp).getComponents()) {
-                if (!(child instanceof ComponentInst)) {
-                    collectEnumDecls((ComplexComponent)child, enumDecls);
-                }
-            }
-            for (ComponentDef cdef: ((Group)comp).getDefinitions()) {
-                collectEnumDecls(cdef.getBody(), enumDecls);
-            }
+        @Override
+        public Spliterator<ComplexComponent> trySplit() {
+            return null; // Don't allow splitting the stream.
         }
+    }
+
+    /**
+     * Function providing a stream of complex components to process from the root.
+     *
+     * @param comp Root component to traverse.
+     * @return Stream of contained complex component found in the root component.
+     */
+    public static Stream<ComplexComponent> getComplexComponents(ComplexComponent comp) {
+        return StreamSupport.stream(new ComplexComponentSpliterator(comp, false), false);
+    }
+
+    /**
+     * Function providing a stream of complex components to process from the root.
+     *
+     * @param comp Root component to traverse.
+     * @param traverseCompDefs Whether to search component definitions as well.
+     * @return Stream of contained complex component found in the root component.
+     */
+    public static Stream<ComplexComponent> getComplexComponents(ComplexComponent comp, boolean traverseCompDefs) {
+        return StreamSupport.stream(new ComplexComponentSpliterator(comp, traverseCompDefs), false);
     }
 }
