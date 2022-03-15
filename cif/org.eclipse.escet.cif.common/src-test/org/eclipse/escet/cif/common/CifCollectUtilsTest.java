a645a4c38c501da210b3922fe98a21653facc39d
Albert Hofkamp <a.t.hofkamp@tue.nl>
2022-03-15 15:50:37 +0100

    #330 Add tests for the stream generator.

---
+++ cif/org.eclipse.escet.cif.common/src-test/org/eclipse/escet/cif/common/CifCollectUtilsTest.java
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.common;
+
+import static org.eclipse.escet.cif.common.CifCollectUtils.getComplexComponents;
+import static org.eclipse.escet.cif.metamodel.java.CifConstructors.newAutomaton;
+import static org.eclipse.escet.cif.metamodel.java.CifConstructors.newGroup;
+import static org.eclipse.escet.cif.metamodel.java.CifConstructors.newSpecification;
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.escet.cif.metamodel.cif.Group;
+import org.eclipse.escet.cif.metamodel.cif.Specification;
+import org.junit.Test;
+
+/** Tests for the {@link CifCollectUtils} class. */
+public class CifCollectUtilsTest {
+    /** Streaming test with an empty specification. */
+    @Test
+    public void testEmptyComponentStream() {
+        Specification spec = newSpecification();
+
+        long numComponents = getComplexComponents(spec).count();
+        assertEquals(1, numComponents);
+    }
+
+    /** Streaming test with a few nested complex components. */
+    @Test
+    public void testNestedComponentStream() {
+        Specification spec = newSpecification();
+        Group grp1 = newGroup();
+        spec.getComponents().add(grp1);
+        spec.getComponents().add(newAutomaton());
+        Group grp2 = newGroup();
+        grp1.getComponents().add(grp2);
+
+        long numComponents = getComplexComponents(spec).count();
+        assertEquals(4, numComponents);
+    }
+}
