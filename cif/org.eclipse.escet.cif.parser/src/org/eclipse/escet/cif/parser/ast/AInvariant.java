4fb0b658a809ce47231e1ae50be3abc2b8f4f146
Ferdie Reijnen <ferdie.reijnen@gmail.com>
2022-03-19 10:46:30 +0100

    #323 Updated parser to allow named invariants.

--- cif/org.eclipse.escet.cif.parser/src/org/eclipse/escet/cif/parser/ast/AInvariant.java
+++ cif/org.eclipse.escet.cif.parser/src/org/eclipse/escet/cif/parser/ast/AInvariant.java
@@ -16,6 +16,7 @@ package org.eclipse.escet.cif.parser.ast;
 import java.util.List;
 
 import org.eclipse.escet.cif.parser.ast.expressions.AExpression;
+import org.eclipse.escet.cif.parser.ast.tokens.AIdentifier;
 import org.eclipse.escet.cif.parser.ast.tokens.AName;
 import org.eclipse.escet.common.java.Assert;
 import org.eclipse.escet.common.position.metamodel.position.Position;
@@ -23,6 +24,9 @@ import org.eclipse.escet.setext.runtime.Token;
 
 /** Invariant. */
 public class AInvariant extends ACifObject {
+    /** The name, or {@code null} if not specified. */
+    public final AIdentifier name;
+
     /** The predicate. */
     public final AExpression predicate;
 
@@ -35,14 +39,16 @@ public class AInvariant extends ACifObject {
     /**
      * Constructor for the {@link AInvariant} class.
      *
+     * @param name The name.
      * @param predicate The predicate.
      * @param invKind The invariant kind. Is either a state/event exclusion kind token, or {@code null} for state
      *     invariants.
      * @param events The textual references to the events, or {@code null} for state invariants.
      * @param position Position information.
      */
-    public AInvariant(AExpression predicate, Token invKind, List<AName> events, Position position) {
+    public AInvariant(AIdentifier name, AExpression predicate, Token invKind, List<AName> events, Position position) {
         super(position);
+        this.name = name;
         this.predicate = predicate;
         this.invKind = invKind;
         this.events = events;
