b8693855de57bd4036746f8bbf7b73731e1a7033
Dennis Hendriks <dh_tue@hotmail.com>
2022-06-14 20:48:01 +0200

    #196 Move Graph.fromString to GraphTest class.

--- cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/varorder/graph/Graph.java
+++ cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/varorder/graph/Graph.java
@@ -17,9 +17,7 @@ import static org.eclipse.escet.common.java.BitSets.bitset;
 import static org.eclipse.escet.common.java.Lists.listc;
 import static org.eclipse.escet.common.java.Pair.pair;
 import static org.eclipse.escet.common.java.Sets.list2set;
-import static org.eclipse.escet.common.java.Strings.fmt;
 
-import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Comparator;
 import java.util.List;
@@ -153,45 +151,6 @@ public class Graph {
             }
         }
         return newGraph;
-    }
-
-    /**
-     * Constructs a graph from the given text, for testing purposes.
-     *
-     * @param text The text. Each line is considered a row. Each character is an element in the row. A '{@code .}' is
-     *     interpreted as zero. Any other character must be a single digit positive weight value.
-     * @return The graph.
-     * @throws AssertionError If the text is not valid.
-     * @throws IllegalArgumentException If the text is not valid.
-     */
-    static Graph fromString(String text) {
-        List<String> lines = Arrays.asList(text.replace("\r", "").split("\n"));
-        Graph graph = new Graph(lines.size());
-        for (int i = 0; i < lines.size(); i++) {
-            String line = lines.get(i);
-            Assert.areEqual(lines.size(), line.length());
-            for (int j = 0; j < line.length(); j++) {
-                char c = line.charAt(j);
-
-                // Get weight.
-                int weight;
-                if (c == '.') {
-                    weight = 0;
-                } else if (c >= '1' && c <= '9') {
-                    weight = c - '0';
-                } else {
-                    throw new IllegalArgumentException(fmt("Invalid weight \"%s\".", c));
-                }
-
-                // Add edge if non-zero weight.
-                if (i == j) { // Diagonal.
-                    Assert.areEqual(0, weight); // Diagonal must be empty.
-                } else if (weight > 0) {
-                    graph.nodes.get(i).addEdge(graph.nodes.get(j), weight);
-                }
-            }
-        }
-        return graph;
     }
 
     @Override
