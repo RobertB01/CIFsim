9d4a9abd8b2052f86710a6ad620c80c117fe0235
Dennis Hendriks <dh_tue@hotmail.com>
2022-06-18 14:12:14 +0200

    #196 Removed ArrayUtils.reverse.

--- common/org.eclipse.escet.common.java/src-test/org/eclipse/escet/common/java/ArrayUtilsTest.java
+++ common/org.eclipse.escet.common.java/src-test/org/eclipse/escet/common/java/ArrayUtilsTest.java
@@ -14,11 +14,8 @@
 package org.eclipse.escet.common.java;
 
 import static org.eclipse.escet.common.java.ArrayUtils.array;
-import static org.eclipse.escet.common.java.ArrayUtils.reverse;
 import static org.eclipse.escet.common.java.Lists.list;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
 
 import java.util.Arrays;
 import java.util.List;
@@ -47,39 +44,5 @@ public class ArrayUtilsTest {
 
         assertEquals("[[1, 2], []]", Arrays.deepToString(al1));
         assertEquals("[]", Arrays.deepToString(al2));
-    }
-
-    @Test
-    @SuppressWarnings("javadoc")
-    public void testReverse() {
-        String[] empty = array();
-        Integer[] i123 = array(1, 2, 3);
-        List<Integer>[] list = array(list(1, 2), list());
-
-        String[] reverseEmpty = reverse(empty);
-        Integer[] reverseI123 = reverse(i123);
-        List<Integer>[] reverseList = reverse(list);
-
-        assertNotSame(empty, reverseEmpty);
-        assertNotSame(i123, reverseI123);
-        assertNotSame(list, reverseList);
-
-        assertEquals(0, reverseEmpty.length);
-        assertEquals(3, reverseI123.length);
-        assertEquals(2, reverseList.length);
-
-        assertEquals(3, (int)reverseI123[0]);
-        assertEquals(2, (int)reverseI123[1]);
-        assertEquals(1, (int)reverseI123[2]);
-
-        assertEquals(list(), reverseList[0]);
-        assertEquals(list(1, 2), reverseList[1]);
-
-        assertSame(i123[0], reverseI123[2]);
-        assertSame(i123[1], reverseI123[1]);
-        assertSame(i123[2], reverseI123[0]);
-
-        assertSame(list[0], reverseList[1]);
-        assertSame(list[1], reverseList[0]);
     }
 }
