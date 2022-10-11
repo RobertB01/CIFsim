436de90c7ebb3eccfa3af43cfcff4c9e385be8fa
Albert Hofkamp <a.t.hofkamp@tue.nl>
2022-10-11 15:46:01 +0200

    #368 Add EqnNotAllowedCheck checker.

---
+++ cif/org.eclipse.escet.cif.common/src/org/eclipse/escet/cif/common/checkers/checks/EqnNotAllowedCheck.java
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.common.checkers.checks;
+
+import org.eclipse.escet.cif.common.checkers.CifCheck;
+import org.eclipse.escet.cif.common.checkers.CifCheckViolations;
+import org.eclipse.escet.cif.common.checkers.messages.LiteralMessage;
+import org.eclipse.escet.cif.common.checkers.messages.ReportObjectTypeDescrMessage;
+import org.eclipse.escet.cif.metamodel.cif.Equation;
+
+/** CIF check not allowing equations. */
+public class EqnNotAllowedCheck extends CifCheck {
+    @Override
+    protected void preprocessEquation(Equation obj, CifCheckViolations arg) {
+        arg.add(obj, new ReportObjectTypeDescrMessage(), new LiteralMessage("has an equation"));
+    }
+}
