d21345f94be8305d1e91bffe3d1e73f6cff78cca
Albert Hofkamp <a.t.hofkamp@tue.nl>
2022-11-15 10:45:27 +0100

    #424 Add check to disallow specific statements in internal user defined functions.

---
+++ cif/org.eclipse.escet.cif.tests/src-test/org/eclipse/escet/cif/tests/common/checkers/FuncNoSpecificIntUserDefFuncStatsCheckWhile.java
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.tests.common.checkers;
+
+import org.eclipse.escet.cif.common.checkers.checks.FuncNoSpecificIntUserDefFuncStatsCheck;
+
+/** Test class for {@link FuncNoSpecificIntUserDefFuncStatsCheck} to check the partial assignments check. */
+public class FuncNoSpecificIntUserDefFuncStatsCheckWhile extends FuncNoSpecificIntUserDefFuncStatsCheck {
+    /** Constructor of the {@link FuncNoSpecificIntUserDefFuncStatsCheckWhile} class. */
+    public FuncNoSpecificIntUserDefFuncStatsCheckWhile() {
+        super(NoSpecificStatement.WHILE);
+    }
+}
