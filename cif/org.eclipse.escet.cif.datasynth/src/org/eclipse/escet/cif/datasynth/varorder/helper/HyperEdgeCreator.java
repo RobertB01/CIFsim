89efba97f039624077dd351f4de732c8b1272e7f
Dennis Hendriks <dh_tue@hotmail.com>
2022-11-19 20:52:15 +0100

    #380 HyperEdgeCreator as proper base class.
    - Abstract method, similar to LegacyHyperEdgeCreator.
    - More arguments for addHyperEdge to be independent of fields.
    - Bugfix for an assertion.
    - Give an assertion with more information in case of violation.

--- cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/varorder/helper/HyperEdgeCreator.java
+++ cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/varorder/helper/HyperEdgeCreator.java
@@ -14,36 +14,53 @@
 package org.eclipse.escet.cif.datasynth.varorder.helper;
 
 import java.util.BitSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.escet.cif.datasynth.spec.SynthesisDiscVariable;
 import org.eclipse.escet.cif.datasynth.spec.SynthesisInputVariable;
 import org.eclipse.escet.cif.datasynth.spec.SynthesisLocPtrVariable;
 import org.eclipse.escet.cif.datasynth.spec.SynthesisVariable;
+import org.eclipse.escet.cif.metamodel.cif.Specification;
 import org.eclipse.escet.common.java.Assert;
 import org.eclipse.escet.common.position.metamodel.position.PositionObject;
 
-public class HyperEdgeCreator {
+/** Automatic variable ordering hyper-edge creator. */
+abstract class HyperEdgeCreator {
+    /**
+     * Creates and returns hyper-edges for the given CIF specification.
+     *
+     * @param spec The CIF specification. Must not be modified.
+     * @param variables The synthesis variables.
+     * @return The hyper-edges.
+     */
+    abstract List<BitSet> getHyperEdges(Specification spec, List<SynthesisVariable> variables);
+
     /**
      * Add a hyper-edge for the given CIF variable objects. Creating and adding a hyper-edge is skipped if no CIF
      * variable objects are provided.
      *
-     * @param vars The CIF variable objects.
+     * @param variables The synthesis variables in the order in which they occur in a hyper-edge.
+     * @param edgeVars The CIF variable objects for which to create a new hyper-edge. This must be a subset of the
+     *     variables represented by {@code variables}.
+     * @param hyperEdges The collection of hyper-edges to which to add the new hyper-edge.
      */
-    private void addHyperEdge(Set<PositionObject> vars) {
+    protected void addHyperEdge(List<SynthesisVariable> variables, Collection<PositionObject> edgeVars,
+            List<BitSet> hyperEdges)
+    {
         // Skip creation of hyper-edges without any variables.
-        if (vars.isEmpty()) {
+        if (edgeVars.isEmpty()) {
             return;
         }
 
         // Create bit set.
         BitSet hyperEdge = new BitSet(variables.size());
-        for (PositionObject var: vars) {
+        for (PositionObject var: edgeVars) {
             int matchIdx = -1;
             for (int i = 0; i < variables.size(); i++) {
                 // Get synthesis variable.
                 SynthesisVariable synthVar = variables.get(i);
-                Assert.notNull(synthVar == null);
+                Assert.notNull(synthVar);
 
                 // Check for matching variable.
                 boolean match = false;
@@ -74,7 +91,7 @@ public class HyperEdgeCreator {
             }
 
             // Must have found a match.
-            Assert.check(matchIdx >= 0);
+            Assert.check(matchIdx >= 0, var);
             hyperEdge.set(matchIdx);
         }
 
