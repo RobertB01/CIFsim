2e40bbfba150a691bdaf10d2f9a450ec7d9877ca
Dennis Hendriks <dh_tue@hotmail.com>
2022-12-10 20:37:25 +0100

    #378 Add RelationsKind to configure legacy vs linearized relations.

---
+++ cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/varorder/helper/RelationsKind.java
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.datasynth.varorder.helper;
+
+import org.eclipse.escet.cif.datasynth.options.BddHyperEdgeAlgoOption;
+
+/** The kind of relations from the CIF specification to use. */
+public enum RelationsKind {
+    /** Use {@link #LEGACY} or {@link #LINEARIZED} based on the configured {@link BddHyperEdgeAlgoOption} value. */
+    CONFIGURED,
+
+    /** Use {@link LegacyHyperEdgeCreator legacy} relations. */
+    LEGACY,
+
+    /** Use {@link LinearizedHyperEdgeCreator linear} relations. */
+    LINEARIZED;
+}
