42eef01782a7709f6934d0667e2ca4edad629bcb
Dennis Hendriks <dh_tue@hotmail.com>
2023-01-03 20:20:45 +0100

    #493 Bit of refactoring of reachability loop.

--- cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/CifDataSynthesis.java
+++ cif/org.eclipse.escet.cif.datasynth/src/org/eclipse/escet/cif/datasynth/CifDataSynthesis.java
@@ -1441,66 +1441,60 @@ public class CifDataSynthesis {
             // Push through all edges.
             for (SynthesisEdge edge: aut.orderedEdges) {
                 // Skip edges if requested.
-                boolean skip = false;
-                if (!ctrl && edge.event.getControllable()) {
+                if ((!ctrl && edge.event.getControllable()) || (!unctrl && !edge.event.getControllable())) {
                     remainingEdges--;
-                    skip = true;
-                } else if (!unctrl && !edge.event.getControllable()) {
-                    remainingEdges--;
-                    skip = true;
+                    if (remainingEdges == 0) {
+                        break; // Fixed point reached.
+                    }
+                    continue;
                 }
                 if (aut.env.isTerminationRequested()) {
                     return null;
                 }
 
-                // Apply the edge, if not skipped.
-                if (!skip) {
-                    // Apply edge. Apply the runtime error predicates when applying backward.
-                    BDD updPred = pred.id();
-                    updPred = edge.apply(updPred, bad, forward, restriction, !forward);
-                    if (aut.env.isTerminationRequested()) {
-                        return null;
-                    }
-
-                    // Extend reachable states.
-                    BDD newPred = pred.id().orWith(updPred);
-                    if (aut.env.isTerminationRequested()) {
-                        return null;
-                    }
-
-                    // Detect change.
-                    if (pred.equals(newPred)) {
-                        // No change.
-                        newPred.free();
-                        remainingEdges--;
-                    } else {
-                        // Change.
-                        if (aut.env.isTerminationRequested()) {
-                            return null;
-                        }
-                        if (dbgEnabled) {
-                            String restrTxt;
-                            if (restriction == null) {
-                                restrTxt = "";
-                            } else {
-                                Assert.notNull(restrictionName);
-                                restrTxt = fmt(", restricted to %s predicate: %s", restrictionName,
-                                        bddToStr(restriction, aut));
-                            }
-                            dbg("%s: %s -> %s [%s reach with edge: %s%s]", Strings.makeInitialUppercase(predName),
-                                    bddToStr(pred, aut), bddToStr(newPred, aut), (forward ? "forward" : "backward"),
-                                    edge.toString(0, ""), restrTxt);
-                        }
-                        pred.free();
-                        pred = newPred;
-                        changed = true;
-                        remainingEdges = aut.orderedEdges.size();
-                    }
+                // Apply edge. Apply the runtime error predicates when applying backward.
+                BDD updPred = pred.id();
+                updPred = edge.apply(updPred, bad, forward, restriction, !forward);
+                if (aut.env.isTerminationRequested()) {
+                    return null;
                 }
 
-                // Detect fixed point during iteration.
-                if (remainingEdges == 0) {
-                    break;
+                // Extend reachable states.
+                BDD newPred = pred.id().orWith(updPred);
+                if (aut.env.isTerminationRequested()) {
+                    return null;
+                }
+
+                // Detect change.
+                if (pred.equals(newPred)) {
+                    // No change.
+                    newPred.free();
+                    remainingEdges--;
+                    if (remainingEdges == 0) {
+                        break; // Fixed point reached.
+                    }
+                } else {
+                    // Change.
+                    if (aut.env.isTerminationRequested()) {
+                        return null;
+                    }
+                    if (dbgEnabled) {
+                        String restrTxt;
+                        if (restriction == null) {
+                            restrTxt = "";
+                        } else {
+                            Assert.notNull(restrictionName);
+                            restrTxt = fmt(", restricted to %s predicate: %s", restrictionName,
+                                    bddToStr(restriction, aut));
+                        }
+                        dbg("%s: %s -> %s [%s reach with edge: %s%s]", Strings.makeInitialUppercase(predName),
+                                bddToStr(pred, aut), bddToStr(newPred, aut), (forward ? "forward" : "backward"),
+                                edge.toString(0, ""), restrTxt);
+                    }
+                    pred.free();
+                    pred = newPred;
+                    changed = true;
+                    remainingEdges = aut.orderedEdges.size();
                 }
             }
         }
