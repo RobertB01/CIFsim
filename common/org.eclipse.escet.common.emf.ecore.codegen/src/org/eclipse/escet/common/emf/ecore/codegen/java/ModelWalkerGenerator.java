855011c234c3e04bbed0824b8643802fa20914b6
freijnen <ferdie.reijnen@gmail.com>
2023-01-03 08:10:33 +0100

    #497 Updated year from 2022 to 2023.

--- common/org.eclipse.escet.common.emf.ecore.codegen/src/org/eclipse/escet/common/emf/ecore/codegen/java/ModelWalkerGenerator.java
+++ common/org.eclipse.escet.common.emf.ecore.codegen/src/org/eclipse/escet/common/emf/ecore/codegen/java/ModelWalkerGenerator.java
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2010, 2022 Contributors to the Eclipse Foundation
+// Copyright (c) 2010, 2023 Contributors to the Eclipse Foundation
 //
 // See the NOTICE file(s) distributed with this work for additional
 // information regarding copyright ownership.
@@ -191,7 +191,7 @@ public class ModelWalkerGenerator extends EmfJavaCodeGenerator {
 
         // Add the file header.
         box.add("//////////////////////////////////////////////////////////////////////////////");
-        box.add("// Copyright (c) 2010, 2022 Contributors to the Eclipse Foundation");
+        box.add("// Copyright (c) 2010, 2023 Contributors to the Eclipse Foundation");
         box.add("//");
         box.add("// See the NOTICE file(s) distributed with this work for additional");
         box.add("// information regarding copyright ownership.");
