04dcfa274913a74b177f3d31f23a873a15aacea6
Dennis Hendriks <dh_tue@hotmail.com>
2023-02-20 18:59:27 +0100

    #454 Add checker test for SVG file references and re-parting/tchecking.

---
+++ cif/org.eclipse.escet.cif.tests/src-test/org/eclipse/escet/cif/tests/checkers/checks/SvgFileReferenceTestCheck.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.tests.checkers.checks;
+
+import org.eclipse.escet.cif.common.checkers.checks.ExprNoSpecificExprsCheck;
+
+/**
+ * CIF check with an SVG file reference and SVG output mapping. Tests that the checker framework correctly resolves the
+ * file while re-parsing and re-type-checking the specification.
+ */
+public class SvgFileReferenceTestCheck extends ExprNoSpecificExprsCheck {
+    /** Constructor for the {@link SvgFileReferenceTestCheck} class. */
+    public SvgFileReferenceTestCheck() {
+        super(NoSpecificExpr.BOOL_LITS);
+    }
+}
