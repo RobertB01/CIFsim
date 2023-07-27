9b822c81ef36d71c84fce2aa52e4d0aeabfc4ba8
Albert Hofkamp <a.t.hofkamp@tue.nl>
2023-07-27 16:25:30 +0200

    #631 Add output providers mainly intended for debugging or testing.

---
+++ common/org.eclipse.escet.common.java/src/org/eclipse/escet/common/java/output/StoredOutputProvider.java
@@ -0,0 +1,294 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.common.java.output;
+
+import org.eclipse.escet.common.java.Assert;
+import org.eclipse.escet.common.java.Strings;
+
+/**
+ * Class that stores output lines from debug, normal, warning and/or error output streams.
+ *
+ * <p>
+ * <ol>
+ * <li>Instantiate the class to create output storage facilities. The instance can be queried afterwards to obtain the
+ * stored output.</li>
+ * <li>Create one or more output streams connected to the output storage by calling {@link #getDebugOutput},
+ * {@link #getNormalOutput}, {@link #getWarnOutput}, and/or {@link #getErrorOutput}. All created outputs share the
+ * string storage, output from all created streams is added in the order of producing it.</li>
+ * <li>To get the stored output, use {@link #toString} of the output storage instance.</li>
+ * </ol>
+ * </p>
+ *
+ * <p>
+ * Other notes:
+ * <ul>
+ * <li>The output streams of {@link #getDebugOutput}, {@link #getNormalOutput}, {@link #getWarnOutput} and
+ * {@link #getErrorOutput}.are lazily created and saved. Asking for the same stream more than once will return the same
+ * output stream object on every call. This ensures that indentation levels etc are shared.</li>
+ * <li>To get separate storage of the output for a stream, create an instance of this class dedicated for that
+ * stream.</li>
+ * </ul>
+ * </p>
+ * ]
+ */
+public class StoredOutputProvider {
+    /** Stroage of the produced output. */
+    private final StringBuilder stringStore = new StringBuilder();
+
+    /** Whether debug output is enabled. */
+    private final boolean isDebugEnabled;
+
+    /** Whether normal output is enabled. */
+    private final boolean isNormalEnabled;
+
+    /** Whether warning output is enabled. */
+    private final boolean isWarnEnabled;
+
+    /** Number of space to insert for a single indent level. */
+    private final int indentSize;
+
+    /** Output stream for debug output. Is lazily constructed. */
+    private DebugNormalOutput debugOutput = null;
+
+    /** Output stream for normal output. Is lazily constructed. */
+    private DebugNormalOutput normalOutput = null;
+
+    /** Output stream for waning output. Is lazily constructed. */
+    private WarnOutput warnOutput = null;
+
+    /** Output stream for errpr output. Is lazily constructed. */
+    private ErrorOutput errorOutput = null;
+
+    /** Constructor of the {@link StoredOutputProvider} class. */
+    public StoredOutputProvider() {
+        this(true, true, true, 4);
+    }
+
+    /**
+     * Constructor of the {@link StoredOutputProvider} class.
+     *
+     * @param isDebugEnabled Whether debug output is enabled.
+     * @param isNormalEnabled Whether normal output is enabled.
+     * @param isWarnEnabled Whether warning output is enabled.
+     */
+    public StoredOutputProvider(boolean isDebugEnabled, boolean isNormalEnabled, boolean isWarnEnabled) {
+        this(isDebugEnabled, isNormalEnabled, isWarnEnabled, 4);
+    }
+
+    /**
+     * Constructor of the {@link StoredOutputProvider} class.
+     *
+     * @param isDebugEnabled Whether debug output is enabled.
+     * @param isNormalEnabled Whether normal output is enabled.
+     * @param isWarnEnabled Whether warning output is enabled.
+     * @param indentSize Number of space to insert for a single indent level.
+     */
+    public StoredOutputProvider(boolean isDebugEnabled, boolean isNormalEnabled, boolean isWarnEnabled, int indentSize) {
+        this.isDebugEnabled = isDebugEnabled;
+        this.isNormalEnabled = isNormalEnabled;
+        this.isWarnEnabled = isWarnEnabled;
+        this.indentSize = indentSize;
+    }
+
+    /**
+     * Get the debug output stream for this string storage. Every call returns the same instance.
+     *
+     * <p>
+     * This function sets the prefix to {@code "DBG: "} if no debug output stream has been created before.
+     * </p>
+     *
+     * @return The constructed debug output stream.
+     */
+    public DebugNormalOutput getDebugOutput() {
+        return getDebugOutput("DBG: ");
+    }
+
+    /**
+     * Get the debug output stream for this string storage. Every call returns the same instance.
+     *
+     * @param linePrefix Prefix text added before a debug line. May be {@code null} to disable a prefix. Is used only
+     *     during the first call since every next call will return the same instance.
+     * @return The constructed debug output stream.
+     */
+    public DebugNormalOutput getDebugOutput(String linePrefix) {
+        if (debugOutput == null) {
+            debugOutput = makeDebugNormalOutput(isDebugEnabled, linePrefix);
+        }
+        return debugOutput;
+    }
+
+    /**
+     * Get the normal output stream for this string storage. Every call returns the same instance.
+     *
+     * <p>
+     * This function sets an empty prefix if no debug output stream has been created before.
+     * </p>
+     *
+     * @return The constructed normal output stream.
+     */
+    public DebugNormalOutput getNormalOutput() {
+        return getNormalOutput(null);
+    }
+
+    /**
+     * Get the normal output stream for this string storage. Every call returns the same instance.
+     *
+     * @param linePrefix Prefix text added before a normal line. May be {@code null} to disable a prefix. Is used only
+     *     during the first call since every next call will return the same instance.
+     * @return The constructed normal output stream.
+     */
+    public DebugNormalOutput getNormalOutput(String linePrefix) {
+        if (normalOutput == null) {
+            normalOutput = makeDebugNormalOutput(isNormalEnabled, linePrefix);
+        }
+        return normalOutput;
+    }
+
+    /**
+     * Construct a debug or normal output stream.
+     *
+     * @param isEnabled Whether the stream is enabled.
+     * @param linePrefix Prefix text added before a line. May be {@code null} to disable a prefix.
+     * @return The constructed output stream.
+     */
+    private DebugNormalOutput makeDebugNormalOutput(boolean isEnabled, String linePrefix) {
+        return new DebugNormalOutput() {
+            /** Indentation level of the output stream. */
+            private int indentLevel = 0;
+
+            /** Cached indentation string, {@code null} means it must be recomputed. */
+            private String curIndentText = "";
+
+            @Override
+            public void line(String message) {
+                // Update the indentation string if necessary.
+                if (curIndentText == null) {
+                    curIndentText = Strings.spaces(indentLevel * indentSize);
+                }
+                if (linePrefix != null && !linePrefix.isEmpty()) {
+                    message = linePrefix + curIndentText + message + "\n";
+                } else {
+                    message = curIndentText + message + "\n";
+                }
+                stringStore.append(message);
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return isEnabled;
+            }
+
+            @Override
+            public void inc() {
+                if (isEnabled()) {
+                    indentLevel++;
+                    curIndentText = null;
+                }
+            }
+
+            @Override
+            public void dec() {
+                if (isEnabled()) {
+                    Assert.check(indentLevel > 0);
+                    indentLevel--;
+                    curIndentText = null;
+                }
+            }
+        };
+    }
+
+    /**
+     * Get the warn output stream for this string storage. Every call returns the same instance.
+     *
+     * <p>
+     * This function sets the prefix to {@code "WARN: "} if no warn output stream has been created before.
+     * </p>
+     *
+     * @return The constructed warn output stream.
+     */
+    public WarnOutput geWarnOutput() {
+        return getWarnOutput("WARN");
+    }
+
+    /**
+     * Get the warn output stream for this string storage. Every call returns the same instance.
+     *
+     * @param linePrefix Prefix text added before a warn line. May be {@code null} to disable a prefix. Is used only
+     *     during the first call since every next call will return the same instance.
+     * @return The constructed warn output stream.
+     */
+    public WarnOutput getWarnOutput(String linePrefix) {
+        if (warnOutput == null) {
+            warnOutput = new WarnOutput() {
+                @Override
+                public void line(String message) {
+                    if (linePrefix != null && !linePrefix.isEmpty()) {
+                        message = linePrefix + message + "\n";
+                    } else {
+                        message = message + "\n";
+                    }
+                    stringStore.append(message);
+                }
+
+                @Override
+                public boolean isEnabled() {
+                    return isWarnEnabled;
+                }
+            };
+        }
+        return warnOutput;
+    }
+
+    /**
+     * Get the error output stream for this string storage. Every call returns the same instance.
+     *
+     * <p>
+     * This function sets the prefix to {@code "ERROR: "} if no error output stream has been created before.
+     * </p>
+     *
+     * @return The constructed error output stream.
+     */
+    public ErrorOutput getErrorOutput() {
+        return getErrorOutput("ERROR: ");
+    }
+
+    /**
+     * Get the error output stream for this string storage. Every call returns the same instance.
+     *
+     * @param linePrefix Prefix text added before an error line. May be {@code null} to disable a prefix. Is used only
+     *     during the first call since every next call will return the same instance.
+     * @return The constructed error output stream.
+     */
+    public ErrorOutput getErrorOutput(String linePrefix) {
+        if (errorOutput == null) {
+            errorOutput = new ErrorOutput() {
+                @Override
+                public void line(String message) {
+                    if (linePrefix != null && !linePrefix.isEmpty()) {
+                        message = linePrefix + message + "\n";
+                    } else {
+                        message = message + "\n";
+                    }
+                    stringStore.append(message);
+                }
+            };
+        }
+        return errorOutput;
+    }
+
+    @Override
+    public String toString() {
+        return stringStore.toString();
+    }
+}
