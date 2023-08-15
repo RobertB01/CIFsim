aaa08a74ae1b9b1f6d82cf8ff410ab1e04b3494c
Albert Hofkamp <a.t.hofkamp@tue.nl>
2023-08-15 14:17:57 +0200

    #632 Add search of cells with child group rows and localnodes columns.

--- cif/org.eclipse.escet.cif.multilevel/src/org/eclipse/escet/cif/multilevel/clustering/ComputeMultiLevelTree.java
+++ cif/org.eclipse.escet.cif.multilevel/src/org/eclipse/escet/cif/multilevel/clustering/ComputeMultiLevelTree.java
@@ -246,21 +246,21 @@ public class ComputeMultiLevelTree {
 
         // Lines 6-17, 'M' in the paper contains all child groups. The clustering implementation however moves singleton
         // nodes in the cluster to local nodes. They must be dealt with separately. As a result line 6 expends into
-        // three double nested searches: singleton-singleton, singleton-multi, and multi-mult. Note that both loops
-        // cover the full range so you get all (a, b) and (b, a) pairs that exist.
-        // Having 3 searches also means getting lines 8-14 three times (not all lines are needed). To avoid code
+        // four double nested searches: singleton-singleton, singleton-multi, and multi-mult and multi-singleton. Note
+        // that both loops cover the full range so you get all (a, b) and (b, a) pairs that exist.
+        // Having 4 searches also means getting lines 8-14 three times (not all lines are needed). To avoid code
         // duplication this has been factored out to 'update'.
         if (grp.localNodes != null) {
             for (int node1: new BitSetIterator(grp.localNodes)) {
                 // Check against other local nodes.
                 for (int node2: new BitSetIterator(grp.localNodes)) {
-                    update(groupContent, node1, node2);
+                    update(groupContent, node1, node2); // Compare local nodes against each other.
                 }
 
                 // Check local node against child groups.
                 for (Group childGroup: grp.childGroups) {
                     for (int node2: new BitSetIterator(childGroup.members)) {
-                        update(groupContent, node1, node2);
+                        update(groupContent, node1, node2); // Compare local nodes against nodes of child groups.
                     }
                 }
             }
@@ -279,7 +279,13 @@ public class ComputeMultiLevelTree {
                 Group child2 = grp.childGroups.get(grp2);
                 for (int node1: new BitSetIterator(child1.members)) {
                     for (int node2: new BitSetIterator(child2.members)) {
-                        update(groupContent, node1, node2);
+                        update(groupContent, node1, node2); // Compare nodes of child groups against each other.
+                    }
+
+                    if (grp.localNodes != null) {
+                        for (int node2: new BitSetIterator(grp.localNodes)) {
+                            update(groupContent, node1, node2); // Compare nodes of child groups against local nodes.
+                        }
                     }
                 }
             }
