8c4c8d1226b1b56d07007bcd333dd2951ae82fcd
Dennis Hendriks <dh_tue@hotmail.com>
2023-08-27 16:30:28 +0200

    #632 Move methods from Algo2Data to ComputeMultiLevelTree.
    - Methods are not changed yet. Does not compile. See next commit.

--- cif/org.eclipse.escet.cif.multilevel/src/org/eclipse/escet/cif/multilevel/clustering/ComputeMultiLevelTree.java
+++ cif/org.eclipse.escet.cif.multilevel/src/org/eclipse/escet/cif/multilevel/clustering/ComputeMultiLevelTree.java
@@ -313,6 +313,55 @@ public class ComputeMultiLevelTree {
     }
 
     /**
+     * Find all requirement groups that are related to both plant groups.
+     *
+     * @param plantGrp1 First plant group.
+     * @param plantGrp2 Second plant group.
+     * @return Requirements that relate to both plant groups.
+     */
+    public BitSet collectRequirementsForPlantGroupPair(int plantGrp1, int plantGrp2) {
+        BitSet reqGroups = new BitSet();
+        for (int row = 0; row < rp.getRowDimension(); row++) {
+            if (rp.getEntry(row, plantGrp1) != 0 && rp.getEntry(row, plantGrp2) != 0) {
+                reqGroups.set(row);
+            }
+        }
+        return reqGroups;
+    }
+
+    /**
+     * Collect the plant groups that relate to at least one of the given requirement groups.
+     *
+     * @param reqGroups Requirement groups to match with.
+     * @return The plant groups that relate to at least one of the given requirement groups.
+     */
+    public BitSet collectPlantGroupsForRequirementGroups(BitSet reqGroups) {
+        BitSet plantGroups = new BitSet();
+        for (int col = 0; col < rp.getColumnDimension(); col++) {
+            for (int reqGrp: new BitSetIterator(reqGroups)) {
+                if (rp.getEntry(reqGrp, col) != 0) {
+                    plantGroups.set(col);
+                    break;
+                }
+            }
+        }
+        return plantGroups;
+    }
+
+    /**
+     * Clear the given requirement group rows in the {@link #rp} matrix.
+     *
+     * @param reqGroups Requirement rows to clear.
+     */
+    public void clearPlantGroupsOfRequirementGroups(BitSet reqGroups) {
+        for (int row: new BitSetIterator(reqGroups)) {
+            for (int col = 0; col < rp.getColumnDimension(); col++) {
+                rp.setEntry(row, col, 0);
+            }
+        }
+    }
+
+    /**
      * Compute the results of a call to Algorithm 2 as described in Goorden 2020 for a group with one node (the
      * {@code size(M) = 1} case).
      *
