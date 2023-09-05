f0d173142b40d05f3beb8f6b440c4379d44faa57
Dennis Hendriks <dh_tue@hotmail.com>
2023-09-05 18:12:48 +0200

    #593 Added annotation provider type checker tests.

---
+++ cif/org.eclipse.escet.cif.tests/src-test/org/eclipse/escet/cif/tests/annotations/CrashingAnnoProvider.java
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.tests.annotations;
+
+import org.eclipse.escet.cif.metamodel.cif.Specification;
+import org.eclipse.escet.cif.metamodel.cif.annotations.Annotation;
+import org.eclipse.escet.cif.typechecker.annotations.AnnotationProblemReporter;
+import org.eclipse.escet.cif.typechecker.annotations.AnnotationProvider;
+
+/**
+ * Annotation provider that crashes. Tests that if the annotation is not used, the provider is not created. Also tests
+ * that if used, the error is properly reported.
+ */
+public class CrashingAnnoProvider extends AnnotationProvider {
+    /**
+     * Constructor for the {@link AnnotationProvider} class.
+     *
+     * @param annotationName The name of the annotations handled by this annotation provider.
+     */
+    public CrashingAnnoProvider(String annotationName) {
+        super(annotationName);
+        throw new RuntimeException("Crash!");
+    }
+
+    @Override
+    public void checkAnnotation(Annotation annotation, AnnotationProblemReporter reporter) {
+        // Not used.
+    }
+
+    @Override
+    public void checkGlobal(Specification spec, AnnotationProblemReporter reporter) {
+        // Not used.
+    }
+}
---
+++ cif/org.eclipse.escet.cif.tests/src-test/org/eclipse/escet/cif/tests/annotations/MultiAnnotationProvider2.java
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.tests.annotations;
+
+import org.eclipse.escet.cif.metamodel.cif.Specification;
+import org.eclipse.escet.cif.metamodel.cif.annotations.Annotation;
+import org.eclipse.escet.cif.typechecker.annotations.AnnotationProblemReporter;
+import org.eclipse.escet.cif.typechecker.annotations.AnnotationProvider;
+
+/** Second annotation provider for the 'cif:typechecker:tests:anno:multi' annotation. */
+public class MultiAnnotationProvider2 extends AnnotationProvider {
+    /**
+     * Constructor for the {@link MultiAnnotationProvider2} class.
+     *
+     * @param annotationName The name of the annotations handled by this annotation provider.
+     */
+    public MultiAnnotationProvider2(String annotationName) {
+        super(annotationName);
+    }
+
+    @Override
+    public void checkAnnotation(Annotation annotation, AnnotationProblemReporter reporter) {
+        // Check nothing.
+    }
+
+    @Override
+    public void checkGlobal(Specification spec, AnnotationProblemReporter reporter) {
+        // Check nothing.
+    }
+}
