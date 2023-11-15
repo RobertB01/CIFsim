9bdb51c093950dc8485bd7adfbe492eb26ef5602
Albert Hofkamp <a.t.hofkamp@tue.nl>
2023-11-15 12:44:10 +0100

    #679 Remove TwinCAT notation restrictions until hard proof turns up.

--- cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/targets/TwinCatTarget.java
+++ cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/targets/TwinCatTarget.java
@@ -13,24 +13,6 @@
 
 package org.eclipse.escet.cif.plcgen.targets;
 
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.COMPLEMENT_OP;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.POWER_OP;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_ABS;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_ACOS;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_ASIN;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_ATAN;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_COS;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_EXP;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_LN;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_LOG;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_SIN;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_SQRT;
-import static org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation.STDLIB_TAN;
-
-import java.util.EnumSet;
-
-import org.eclipse.escet.cif.plcgen.model.functions.PlcBasicFuncDescription.PlcFuncNotation;
-import org.eclipse.escet.cif.plcgen.model.functions.PlcFuncOperation;
 import org.eclipse.escet.cif.plcgen.writers.TwinCatWriter;
 import org.eclipse.escet.cif.plcgen.writers.Writer;
 
@@ -59,30 +41,6 @@ public class TwinCatTarget extends PlcBaseTarget {
     @Override
     public boolean supportsEnumerations() {
         return true;
-    }
-
-    @Override
-    public EnumSet<PlcFuncNotation> getSupportedFuncNotations(PlcFuncOperation funcOper, int numArgs) {
-        EnumSet<PlcFuncNotation> funcSupport = super.getSupportedFuncNotations(funcOper, numArgs);
-
-        // TwinCAT 3.1 does not support "**" for pow(a, b).
-        if (funcOper.equals(POWER_OP)) {
-            funcSupport = EnumSet.copyOf(funcSupport);
-            funcSupport.remove(PlcFuncNotation.INFIX);
-            return funcSupport;
-        }
-
-        // TwinCAT does not allow formal function call syntax for the following functions and not for functions with two
-        // or more parameters.
-        EnumSet<PlcFuncOperation> notFormalFuncs = EnumSet.of(COMPLEMENT_OP, STDLIB_ABS, STDLIB_EXP, STDLIB_SQRT,
-                STDLIB_LN, STDLIB_LOG, STDLIB_ACOS, STDLIB_ASIN, STDLIB_ATAN, STDLIB_COS, STDLIB_SIN, STDLIB_TAN);
-        if (notFormalFuncs.contains(funcOper) || numArgs >= 2) {
-            funcSupport = EnumSet.copyOf(funcSupport);
-            funcSupport.remove(PlcFuncNotation.FORMAL);
-            return funcSupport;
-        }
-
-        return funcSupport;
     }
 
     @Override
