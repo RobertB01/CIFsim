4148950772a88a3b85f8fbd9cc0236bf1458defe
Dennis Hendriks <dh_tue@hotmail.com>
2023-11-21 21:27:21 +0100

    #687 Move annotation providers for builtin annotations to sub-package.

--- cif/org.eclipse.escet.cif.typechecker/src/org/eclipse/escet/cif/typechecker/annotations/builtin/DocAnnotationProvider.java
+++ cif/org.eclipse.escet.cif.typechecker/src/org/eclipse/escet/cif/typechecker/annotations/builtin/DocAnnotationProvider.java
@@ -11,7 +11,7 @@
 // SPDX-License-Identifier: MIT
 //////////////////////////////////////////////////////////////////////////////
 
-package org.eclipse.escet.cif.typechecker.annotations;
+package org.eclipse.escet.cif.typechecker.annotations.builtin;
 
 import static org.eclipse.escet.common.java.Strings.fmt;
 
@@ -26,6 +26,8 @@ import org.eclipse.escet.cif.metamodel.cif.annotations.Annotation;
 import org.eclipse.escet.cif.metamodel.cif.annotations.AnnotationArgument;
 import org.eclipse.escet.cif.metamodel.cif.types.CifType;
 import org.eclipse.escet.cif.metamodel.cif.types.StringType;
+import org.eclipse.escet.cif.typechecker.annotations.AnnotationProblemReporter;
+import org.eclipse.escet.cif.typechecker.annotations.AnnotationProvider;
 import org.eclipse.escet.common.app.framework.exceptions.InvalidModelException;
 import org.eclipse.escet.common.typechecker.SemanticProblemSeverity;
 
