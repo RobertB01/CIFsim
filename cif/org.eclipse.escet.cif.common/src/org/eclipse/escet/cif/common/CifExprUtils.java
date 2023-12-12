7e1d950ac6e4099d0211956e049fe8cd06127666
freijnen <ferdie.reijnen@gmail.com>
2023-12-12 18:49:07 +0100

    #542 First attampt at hashExpr method.

---
+++ cif/org.eclipse.escet.cif.common/src/org/eclipse/escet/cif/common/CifExprUtils.java
@@ -0,0 +1,190 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.common;
+
+import static org.eclipse.escet.cif.common.CifTypeUtils.hashType;
+
+import org.eclipse.escet.cif.metamodel.cif.expressions.AlgVariableExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.BinaryExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.BoolExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.CastExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.CompInstWrapExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.CompParamExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.CompParamWrapExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ComponentExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ConstantExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ContVariableExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.DictExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.DictPair;
+import org.eclipse.escet.cif.metamodel.cif.expressions.DiscVariableExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ElifExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.EnumLiteralExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.EventExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.Expression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.FieldExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.FunctionCallExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.FunctionExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.IfExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.InputVariableExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.IntExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ListExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.LocationExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ProjectionExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.RealExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.ReceivedExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.SelfExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.SetExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.SliceExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.StdLibFunctionExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.StringExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.SwitchCase;
+import org.eclipse.escet.cif.metamodel.cif.expressions.SwitchExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.TauExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.TimeExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.TupleExpression;
+import org.eclipse.escet.cif.metamodel.cif.expressions.UnaryExpression;
+
+/** CIF expressions utility methods. */
+public class CifExprUtils {
+    /** Constructor for the {@link CifExprUtils} class. */
+    private CifExprUtils() {
+        // Static class.
+    }
+
+    /**
+     * Hashes the given expression.
+     *
+     * <p>
+     * Component types and component definition types are currently not supported by this method.
+     * </p>
+     *
+     * @param expr The expression.
+     * @return The hash of the expression.
+     */
+    public static int hashExpr(Expression expr) {
+        if (expr instanceof BoolExpression bExpr) {
+            return (bExpr.isValue() ? 1231 : 1237); // TODO: Same has as for Boolean objects, is it a good hash?
+        } else if (expr instanceof IntExpression iExpr) {
+            return iExpr.getValue(); // TODO: Is the integer value a good hash?
+        } else if (expr instanceof RealExpression rExpr) {
+            return rExpr.getValue().hashCode(); // TODO: Default hash for strings, is it a good hash?
+        } else if (expr instanceof StringExpression sExpr) {
+            return sExpr.getValue().hashCode();
+        } else if (expr instanceof TimeExpression tExpr) {
+            return 1 << 0; // TODO: Does this constant make sense?
+        } else if (expr instanceof CastExpression cExpr) {
+            return hashType(cExpr.getType()) + hashExpr(cExpr.getChild());
+        } else if (expr instanceof UnaryExpression uExpr) {
+            return uExpr.getOperator().hashCode() + hashExpr(uExpr.getChild()); // TODO: Uses default hash for enums, is
+                                                                                // it a good hash?
+        } else if (expr instanceof BinaryExpression bExpr) {
+            return hashExpr(bExpr.getLeft()) + bExpr.getOperator().hashCode() + hashExpr(bExpr.getRight());
+        } else if (expr instanceof IfExpression iExpr) {
+            int rslt = 1 << 3; // TODO: Does this constant make sense?
+            for (Expression guard: iExpr.getGuards()) {
+                rslt += hashExpr(guard);
+            }
+            rslt += hashExpr(iExpr.getThen());
+            for (ElifExpression elifExpr: iExpr.getElifs()) {
+                for (Expression guard: elifExpr.getGuards()) {
+                    rslt += hashExpr(guard);
+                }
+                rslt += hashExpr(elifExpr.getThen());
+            }
+            rslt += hashExpr(iExpr.getElse());
+
+            return rslt;
+        } else if (expr instanceof SwitchExpression sExpr) {
+            int rslt = 1 << 6; // TODO: Does this constant make sense?
+            rslt += hashExpr(sExpr.getValue());
+            for (SwitchCase sCase: sExpr.getCases()) {
+                rslt += hashExpr(sCase.getKey()) + hashExpr(sCase.getValue());
+            }
+            return rslt;
+        } else if (expr instanceof ProjectionExpression pExpr) {
+            return hashExpr(pExpr.getChild()) + hashExpr(pExpr.getIndex());
+        } else if (expr instanceof SliceExpression sExpr) {
+            return hashExpr(sExpr.getBegin()) + hashExpr(sExpr.getEnd()) + hashExpr(sExpr.getChild());
+        } else if (expr instanceof FunctionCallExpression fcExpr) {
+            int rslt = 1 << 9; // TODO: Does this constant make sense?
+            rslt += hashExpr(fcExpr.getFunction());
+            for (Expression argument: fcExpr.getArguments()) {
+                rslt += hashExpr(argument);
+            }
+            return rslt;
+        } else if (expr instanceof ListExpression lExpr) {
+            int rslt = 1 << 12; // TODO: Does this constant make sense?
+            for (Expression element: lExpr.getElements()) {
+                rslt += hashExpr(element);
+            }
+            return rslt;
+        } else if (expr instanceof SetExpression sExpr) {
+            int rslt = 1 << 15; // TODO: Does this constant make sense?
+            for (Expression element: sExpr.getElements()) {
+                rslt += hashExpr(element);
+            }
+            return rslt;
+        } else if (expr instanceof TupleExpression tExpr) {
+            int rslt = 1 << 18; // TODO: Does this constant make sense?
+            for (Expression field: tExpr.getFields()) {
+                rslt += hashExpr(field);
+            }
+            return rslt;
+        } else if (expr instanceof DictExpression dExpr) {
+            int rslt = 1 << 21; // TODO: Does this constant make sense?
+            for (DictPair pair: dExpr.getPairs()) {
+                rslt += hashExpr(pair.getKey()) + hashExpr(pair.getValue());
+            }
+            return rslt;
+        } else if (expr instanceof ConstantExpression cExpr) {
+            return cExpr.getConstant().hashCode(); // Uses default hash for objects, is it a good hash?
+        } else if (expr instanceof DiscVariableExpression dvExpr) {
+            return dvExpr.getVariable().hashCode();
+        } else if (expr instanceof AlgVariableExpression aExpr) {
+            return aExpr.getVariable().hashCode();
+        } else if (expr instanceof ContVariableExpression cExpr) {
+            return cExpr.getVariable().hashCode();
+        } else if (expr instanceof InputVariableExpression iExpr) {
+            return iExpr.getVariable().hashCode();
+        } else if (expr instanceof LocationExpression lExpr) {
+            return lExpr.getLocation().hashCode();
+        } else if (expr instanceof TauExpression) {
+            return 1 << 24; // TODO: Does this constant make sense?
+        } else if (expr instanceof EnumLiteralExpression elExpr) {
+            return elExpr.getLiteral().hashCode();
+        } else if (expr instanceof EventExpression eExpr) {
+            return eExpr.getEvent().hashCode();
+        } else if (expr instanceof FieldExpression fExpr) {
+            return fExpr.getField().hashCode();
+        } else if (expr instanceof StdLibFunctionExpression slfExpr) {
+            return slfExpr.getFunction().hashCode();
+        } else if (expr instanceof FunctionExpression fExpr) {
+            return fExpr.getFunction().hashCode();
+        } else if (expr instanceof ComponentExpression cExpr) {
+            return cExpr.getComponent().hashCode();
+        } else if (expr instanceof CompParamExpression cpExpr) {
+            return cpExpr.getParameter().hashCode();
+        } else if (expr instanceof CompInstWrapExpression ciwExpr) {
+            return ciwExpr.getInstantiation().hashCode() + hashExpr(ciwExpr.getReference());
+        } else if (expr instanceof CompParamWrapExpression cpwExpr) {
+            return cpwExpr.getParameter().hashCode() + hashExpr(cpwExpr.getReference());
+        } else if (expr instanceof ReceivedExpression) {
+            return 1 << 27; // TODO: Does this constant make sense?
+        } else if (expr instanceof SelfExpression) {
+            return 1 << 30; // TODO: Does this constant make sense?
+        }
+
+        throw new RuntimeException("Unexpected expression: " + expr.toString());
+    }
+}
