e70ff7cdabbe9098880445f5778e65417b0fe938
Dennis Hendriks <dh_tue@hotmail.com>
2023-12-28 15:27:12 +0100

    #715 CodeContext: proper use of getTargetVariableName vs getTargetRef.

--- cif/org.eclipse.escet.cif.codegen/src/org/eclipse/escet/cif/codegen/CodeContext.java
+++ cif/org.eclipse.escet.cif.codegen/src/org/eclipse/escet/cif/codegen/CodeContext.java
@@ -293,7 +293,7 @@ public class CodeContext {
      * @return The name of the function in the target language.
      */
     public String getFunctionName(InternalFunction func) {
-        return codeGen.getTargetRef(func);
+        return codeGen.getTargetVariableName(func);
     }
 
     /**
