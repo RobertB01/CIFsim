b8cbf5541d957fc089a885a66bb7061551bbef8e
Dennis Hendriks <dh_tue@hotmail.com>
2024-03-17 17:45:06 +0100

    #593 Allow annos on constants: update CIF parser.

--- cif/org.eclipse.escet.cif.parser/src/org/eclipse/escet/cif/parser/CifParserHooks.java
+++ cif/org.eclipse.escet.cif.parser/src/org/eclipse/escet/cif/parser/CifParserHooks.java
@@ -589,9 +589,9 @@ public final class CifParserHooks implements CifParser.Hooks {
         return new AEventDecl(t1, l3, a2, t1.position);
     }
 
-    @Override // Decl : @CONSTKW Type ConstantDefs SEMICOLTK;
-    public ADecl parseDecl07(Token t1, ACifType a2, List<AConstant> l3) {
-        return new AConstDecl(a2, l3, t1.position);
+    @Override // Decl : OptAnnos @CONSTKW Type ConstantDefs SEMICOLTK;
+    public ADecl parseDecl07(List<AAnnotation> l1, Token t2, ACifType a3, List<AConstant> l4) {
+        return new AConstDecl(l1, a3, l4, t2.position);
     }
 
     @Override // Decl : OptAnnos @ALGKW Type AlgVarsDefs SEMICOLTK;
