d70c8238623aa9765601e5ddbcbe526a63ec68c0
Albert Hofkamp <a.t.hofkamp@tue.nl>
2024-04-12 14:38:34 +0200

    #628 Max iter option takes one or two values.

--- cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/options/PlcMaxIterOption.java
+++ cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/options/PlcMaxIterOption.java
@@ -13,11 +13,18 @@
 
 package org.eclipse.escet.cif.plcgen.options;
 
-import org.eclipse.escet.common.app.framework.options.IntegerOption;
+import static org.eclipse.escet.common.java.Strings.fmt;
+
+import java.util.Locale;
+
 import org.eclipse.escet.common.app.framework.options.Options;
+import org.eclipse.escet.common.app.framework.options.StringOption;
 
 /** PLC maximum iterations option. */
-public class PlcMaxIterOption extends IntegerOption {
+public class PlcMaxIterOption extends StringOption {
+    /** Default option value. */
+    private static final String DEFAULT_VALUE = "inf,inf";
+
     /** Constructor for the {@link PlcMaxIterOption} class. */
     public PlcMaxIterOption() {
         super(
@@ -25,9 +32,14 @@ public class PlcMaxIterOption extends IntegerOption {
                 "PLC maximum iterations",
 
                 // description
-                "The maximum number of iterations of the main loop of the main program body, per execution of the "
-                        + "main program body. The number must be positive. Use \"inf\" to allow an infinite number of "
-                        + "iterations (no restriction). [DEFAULT=inf]",
+                """
+                The maximum number of iterations for uncontrollable and controllable events
+                for a single execution of the main program body, given as two values separated by a comma.
+                Each value is either a positive integer number, or "inf" to allow an infinite maximum
+                number of iterations. For example "20,inf" means that in a single execution of the main program body
+                at most 20 uncontrollable events and an unrestricted number of controllable events can be executed.
+                If only one value is given, it is used as maximum for both uncontrollable and controllable events.
+                [DEFAULT=inf,inf]""".replace("\n", " "),
 
                 // cmdShort
                 'x',
@@ -39,16 +51,10 @@ public class PlcMaxIterOption extends IntegerOption {
                 "ITERS",
 
                 // defaultValue
-                null, // Special value 'inf'.
+                DEFAULT_VALUE,
 
-                // minimumValue
-                1,
-
-                // maximumValue
-                Integer.MAX_VALUE,
-
-                // pageIncrementValue
-                10,
+                // defaultAsNull
+                true,
 
                 // showInDialog
                 true,
@@ -58,31 +64,67 @@ public class PlcMaxIterOption extends IntegerOption {
                         + "main program body.",
 
                 // optDialogLabelText
-                "Maximum iterations:",
-
-                // hasSpecialValue
-                true,
-
-                // defaultNormalValue
-                100,
-
-                // specialValueSyntax
-                "inf",
-
-                // optDialogSpecialText
-                "Infinite number of iterations (no restriction)",
-
-                // optDialogNormalText
-                "Finite number of iterations");
+                "Maximum iterations:");
     }
 
     /**
-     * Returns the maximum number of iterations of the main loop of the main program body, per execution of the main
+     * Iteration limits for the uncontrollable and controllable event transition loops.
+     *
+     * @param uncontrollableLimit Maximum number of iterations over the uncontrollalble events. Either a positive number
+     *     or {@code null}. The latter means 'infinite'.
+     * @param controllableLimit Maximum number of iterations over the controllalble events. Either a positive number or
+     *     {@code null}. The latter means 'infinite'.
+     */
+    public static record MaxIterLimits(Integer uncontrollableLimit, Integer controllableLimit) {
+    }
+
+    /**
+     * Returns the maximum number of iterations of the event loops of the main program body, per execution of the main
      * program body.
      *
-     * @return The maximum number of iterations, or {@code null} for no maximum.
+     * @return The maximum number of iterations for uncontrollable and controllable event transition loops.
      */
-    public static Integer getMaxIter() {
-        return Options.get(PlcMaxIterOption.class);
+    public static MaxIterLimits getMaxIterLimits() {
+        String value = Options.get(PlcMaxIterOption.class);
+        if (value == null) {
+            value = DEFAULT_VALUE;
+        }
+
+        int sepIndex = value.indexOf(',');
+        String unconText, conText;
+        if (sepIndex < 0) {
+            unconText = value.strip();
+            conText = value.strip();
+        } else {
+            unconText = value.substring(0, sepIndex).strip();
+            conText = value.substring(sepIndex + 1).strip();
+        }
+        return new MaxIterLimits(convertLimit("uncontrollable", unconText), convertLimit("controllable", conText));
+    }
+
+    /**
+     * Convert the option value string of a limit either to a positive integer number, or to {@code null} for infinite.
+     *
+     * @param context Event context for the user of the number being converted.
+     * @param numberText Text to convert.
+     * @return The converted limit value.
+     */
+    private static Integer convertLimit(String context, String numberText) {
+        if (numberText.toLowerCase(Locale.US).equals("inf")) {
+            return null;
+        } else {
+            int value;
+            try {
+                value = Integer.parseInt(numberText);
+            } catch (NumberFormatException ex) {
+                throw new RuntimeException(fmt("Iteration limit option value \"%s\" for %s events is not recognized.",
+                        numberText, context), ex);
+            }
+            if (value <= 0) {
+                throw new RuntimeException(fmt(
+                        "Iteration limit option value \"%d\" for %s events is not a positive value.", value, context));
+            }
+            return value;
+        }
     }
 }
