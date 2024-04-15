4d37ec216f70a4dece06396ece48e5c0ed6fe98d
Dennis Hendriks <dh_tue@hotmail.com>
2024-04-15 20:18:10 +0200

    #802 Update test output.

--- cif/org.eclipse.escet.cif.tests/tests/codegen/annos_doc_java/annos_doc.java
+++ cif/org.eclipse.escet.cif.tests/tests/codegen/annos_doc_java/annos_doc.java
@@ -10,11 +10,15 @@ import java.util.Objects;
 /**
  * annos_doc code generated from a CIF specification.
  *
+ * <p>
  * First doc
  * with multiple lines.
+ * </p>
  *
+ * <p>
  * Second doc line 1.
  * Second doc line 2.
+ * </p>
  */
 @SuppressWarnings("unused")
 public abstract class annos_doc {
@@ -45,33 +49,43 @@ public abstract class annos_doc {
     /**
      * Constant "constants.c2".
      *
+     * <p>
      * single line doc
+     * </p>
      */
     public static final int constants_c2_ = 2;
 
     /**
      * Constant "constants.c3".
      *
+     * <p>
      * doc with multiple
      * lines of
      *  text
+     * </p>
      */
     public static final int constants_c3_ = 3;
 
     /**
      * Constant "constants.c4".
      *
+     * <p>
      * some doc
+     * </p>
      */
     public static final int constants_c4_ = 4;
 
     /**
      * Constant "constants.c5".
      *
+     * <p>
      * First doc.
+     * </p>
      *
+     * <p>
      * Second doc line 1.
      * Second doc line 2.
+     * </p>
      */
     public static final int constants_c5_ = 5;
 
@@ -388,7 +402,9 @@ public abstract class annos_doc {
     /**
      * Execute code for event "events.e2".
      *
+     * <p>
      * single line doc
+     * </p>
      *
      * @return {@code true} if the event was executed, {@code false} otherwise.
      */
@@ -405,9 +421,11 @@ public abstract class annos_doc {
     /**
      * Execute code for event "events.e3".
      *
+     * <p>
      * doc with multiple
      * lines of
      *  text
+     * </p>
      *
      * @return {@code true} if the event was executed, {@code false} otherwise.
      */
@@ -424,7 +442,9 @@ public abstract class annos_doc {
     /**
      * Execute code for event "events.e4".
      *
+     * <p>
      * some doc
+     * </p>
      *
      * @return {@code true} if the event was executed, {@code false} otherwise.
      */
@@ -441,10 +461,14 @@ public abstract class annos_doc {
     /**
      * Execute code for event "events.e5".
      *
+     * <p>
      * First doc.
+     * </p>
      *
+     * <p>
      * Second doc line 1.
      * Second doc line 2.
+     * </p>
      *
      * @return {@code true} if the event was executed, {@code false} otherwise.
      */
@@ -565,7 +589,9 @@ public abstract class annos_doc {
     /**
      * Evaluates algebraic variable "algvars.a2".
      *
+     * <p>
      * single line doc
+     * </p>
      *
      * @return The evaluation result.
      */
@@ -576,9 +602,11 @@ public abstract class annos_doc {
     /**
      * Evaluates algebraic variable "algvars.a3".
      *
+     * <p>
      * doc with multiple
      * lines of
      *  text
+     * </p>
      *
      * @return The evaluation result.
      */
@@ -589,7 +617,9 @@ public abstract class annos_doc {
     /**
      * Evaluates algebraic variable "algvars.a4".
      *
+     * <p>
      * some doc
+     * </p>
      *
      * @return The evaluation result.
      */
@@ -600,10 +630,14 @@ public abstract class annos_doc {
     /**
      * Evaluates algebraic variable "algvars.a5".
      *
+     * <p>
      * First doc.
+     * </p>
      *
+     * <p>
      * Second doc line 1.
      * Second doc line 2.
+     * </p>
      *
      * @return The evaluation result.
      */
