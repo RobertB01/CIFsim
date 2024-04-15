b007264734811d9018a39bdc6d27a19d7de2aec1
Dennis Hendriks <dh_tue@hotmail.com>
2024-04-15 10:51:49 +0200

    #801 AnnotationEqHashWrap: use regular map instead of Multiset.
    - Also removed the dependency of this plugin on Guava again.

--- cif/org.eclipse.escet.cif.common/src/org/eclipse/escet/cif/common/AnnotationEqHashWrap.java
+++ cif/org.eclipse.escet.cif.common/src/org/eclipse/escet/cif/common/AnnotationEqHashWrap.java
@@ -14,13 +14,11 @@
 package org.eclipse.escet.cif.common;
 
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.eclipse.escet.cif.metamodel.cif.annotations.Annotation;
 import org.eclipse.escet.cif.metamodel.cif.expressions.Expression;
-
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
 
 /** CIF annotation wrapper class, for proper hashing and equality. */
 public class AnnotationEqHashWrap {
@@ -30,8 +28,8 @@ public class AnnotationEqHashWrap {
     /** Per annotation argument name, the evaluated argument value. */
     private final Map<String, Object> argNamesToValues;
 
-    /** The values of the unnamed arguments of the annotation. */
-    private final Multiset<Object> unnamedArgsValues;
+    /** Per unique value of unnamed arguments of the annotation, the number of unnamed arguments that have the value. */
+    private final Map<Object, Long> unnamedArgsValues;
 
     /** The cached hash code of this wrapper. */
     private final int hashCode;
@@ -47,7 +45,7 @@ public class AnnotationEqHashWrap {
                 .collect(Collectors.toMap(arg -> arg.getName(), arg -> evalAnnoArgValue(arg.getValue())));
         this.unnamedArgsValues = annotation.getArguments().stream().filter(arg -> arg.getName() == null)
                 .map(arg -> evalAnnoArgValue(arg.getValue()))
-                .collect(Collectors.toCollection(() -> HashMultiset.create()));
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         this.hashCode = annotation.getName().hashCode() + argNamesToValues.hashCode();
     }
 
@@ -81,8 +79,9 @@ public class AnnotationEqHashWrap {
 
         // Check annotation name and arguments:
         // - We use a map of argument names as the order of the named arguments is not relevant.
-        // - For unnamed arguments, we use a multiset since the order is not relevant, but the number of times that an
-        //   argument is provided may be relevant.
+        // - For unnamed arguments, we use a map as well, as there the order is also not relevant. The map does take
+        //   into account how many arguments have a specific value, since the number of times that an argument is
+        //   provided may be relevant.
         // - And we use evaluated argument values such that for instance values '{1, 2}' and '{2, 1}' are considered
         //   equal.
         return this.annotation.getName().equals(other.annotation.getName())
