58e64beb62f0aa859ef1238c8e8183cb204007c2
Dennis Hendriks <dh_tue@hotmail.com>
2024-05-20 12:27:10 +0200

    #269 Add new CifBddApplyPlantInvariants class to CIF's 'bdd' package.
    - Empty for now.

---
+++ cif/org.eclipse.escet.cif.bdd/src/org/eclipse/escet/cif/bdd/utils/CifBddApplyPlantInvariants.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010, 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.bdd.utils;
+
+import static org.eclipse.escet.cif.bdd.utils.BddUtils.bddToStr;
+
+import org.eclipse.escet.cif.bdd.spec.CifBddEdge;
+import org.eclipse.escet.cif.bdd.spec.CifBddSpec;
+
+import com.github.javabdd.BDD;
+
+/** CIF/BDD helper class to apply plant invariants. */
+public class CifBddApplyPlantInvariants {
+    /** Constructor for the {@link CifBddApplyPlantInvariants} class. */
+    private CifBddApplyPlantInvariants() {
+        // Static class.
+    }
+
+}
