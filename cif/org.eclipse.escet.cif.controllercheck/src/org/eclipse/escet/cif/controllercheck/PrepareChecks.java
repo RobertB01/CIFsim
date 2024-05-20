37b2a827ba5743e1a38f79487a9f78771915c6ed
Dennis Hendriks <dh_tue@hotmail.com>
2024-05-20 13:33:17 +0200

    #621 Change a comment to account for more checks being added later.

--- cif/org.eclipse.escet.cif.controllercheck/src/org/eclipse/escet/cif/controllercheck/PrepareChecks.java
+++ cif/org.eclipse.escet.cif.controllercheck/src/org/eclipse/escet/cif/controllercheck/PrepareChecks.java
@@ -114,7 +114,7 @@ public class PrepareChecks {
         automata = collectAutomata(spec, list());
         Set<Event> allControllableEvents = collectControllableEvents(spec, set());
         if (automata.isEmpty() || allControllableEvents.isEmpty()) {
-            // Both controller check properties trivially hold.
+            // Both finite response and confluence trivially hold.
             return true;
         }
 
