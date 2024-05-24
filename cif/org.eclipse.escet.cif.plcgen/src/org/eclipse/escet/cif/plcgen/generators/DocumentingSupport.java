278fc218520231b2f4ae7253de7e980e64eefe1f
Albert Hofkamp <a.t.hofkamp@tue.nl>
2024-05-24 14:30:58 +0200

    #815 Add utility class for creating comment text.

---
+++ cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/generators/DocumentingSupport.java
@@ -0,0 +1,63 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.plcgen.generators;
+
+import static org.eclipse.escet.cif.common.CifTextUtils.getAbsName;
+
+import org.eclipse.escet.cif.metamodel.cif.automata.Automaton;
+import org.eclipse.escet.cif.metamodel.cif.declarations.ContVariable;
+import org.eclipse.escet.cif.metamodel.cif.declarations.DiscVariable;
+import org.eclipse.escet.cif.metamodel.cif.declarations.Event;
+import org.eclipse.escet.cif.metamodel.cif.declarations.InputVariable;
+import org.eclipse.escet.common.position.metamodel.position.PositionObject;
+
+/** Support class for generating documentation comments for CIF elements. */
+public class DocumentingSupport {
+    /** Constructor of the {@link DocumentingSupport} class. */
+    private DocumentingSupport() {
+        // Static class.
+    }
+
+    /**
+     * Get a description of a CIF element in PLC context. This function supports continuous variables, discrete
+     * variables, input variables and events.
+     *
+     * @param posObj CIF element to describe.
+     * @return The description of the given object.
+     */
+    public static String getDescription(PositionObject posObj) {
+        if (posObj instanceof ContVariable) {
+            return "continuous variable \"" + getAbsName(posObj, false) + "\"";
+        } else if (posObj instanceof DiscVariable dvar) {
+            if (dvar.getName().isEmpty()) { // ElimLocRef transformation introduces variables with enpty name.
+                Automaton aut = (Automaton)posObj.eContainer();
+                return "current location of automaton \"" + getAbsName(aut, false) + "\"";
+            } else {
+                return "discrete variable \"" + getAbsName(posObj, false) + "\"";
+            }
+        } else if (posObj instanceof InputVariable) {
+            return "input variable \"" + getAbsName(posObj, false) + "\"";
+        } else if (posObj instanceof Event evt) {
+            if (evt.getControllable() == null) {
+                return "event \"" + getAbsName(posObj, false) + "\"";
+            } else if (evt.getControllable()) {
+                return "controllable event \"" + getAbsName(posObj, false) + "\"";
+            } else {
+                return "uncontrollable event \"" + getAbsName(posObj, false) + "\"";
+            }
+        } else {
+            throw new AssertionError("Unexpected position object \"" + posObj + "\" found.");
+        }
+    }
+}
