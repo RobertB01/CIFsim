59f6553c3c39f0ee917c20ad4997a9fcbeac2dd7
Albert Hofkamp <a.t.hofkamp@tue.nl>
2024-05-27 18:14:25 +0000

    #621 Apply some suggestions for docs/JavaDocs.

--- cif/org.eclipse.escet.cif.controllercheck/src/org/eclipse/escet/cif/controllercheck/boundedresponse/BoundedResponseCheckConclusion.java
+++ cif/org.eclipse.escet.cif.controllercheck/src/org/eclipse/escet/cif/controllercheck/boundedresponse/BoundedResponseCheckConclusion.java
@@ -22,14 +22,14 @@ import org.eclipse.escet.common.java.Assert;
 /** Conclusion of the bounded response check. */
 public class BoundedResponseCheckConclusion implements CheckConclusion {
     /**
-     * The bound on the number of uncontrollable events per cycle. Is {@code -1} if the system can not be initialized, a
+     * The bound on the number of uncontrollable events per cycle. Is {@code -1} if the system cannot be initialized, a
      * non-negative integer indicating the bound if it has bounded response, and {@code null} if it does not have
      * bounded response.
      */
     private final Integer uncontrollableBound;
 
     /**
-     * The bound on the number of controllable events per cycle. Is {@code -1} if the system can not be initialized, a
+     * The bound on the number of controllable events per cycle. Is {@code -1} if the system cannot be initialized, a
      * non-negative integer indicating the bound if it has bounded response, and {@code null} if it does not have
      * bounded response.
      */
@@ -39,10 +39,10 @@ public class BoundedResponseCheckConclusion implements CheckConclusion {
      * Constructor for the {@link BoundedResponseCheckConclusion} class.
      *
      * @param uncontrollableBound The bound on the number of uncontrollable events per cycle. Is {@code -1} if the
-     *     system can not be initialized, a non-negative integer indicating the bound if it has bounded response, and
+     *     system cannot be initialized, a non-negative integer indicating the bound if it has bounded response, and
      *     {@code null} if it does not have bounded response.
      * @param controllableBound The bound on the number of controllable events per cycle. Is {@code -1} if the system
-     *     can not be initialized, a non-negative integer indicating the bound if it has bounded response, and
+     *     cannot be initialized, a non-negative integer indicating the bound if it has bounded response, and
      *     {@code null} if it does not have bounded response.
      */
     public BoundedResponseCheckConclusion(Integer uncontrollableBound, Integer controllableBound) {
