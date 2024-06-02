8cda573026d0751cfa7b67271ab4ca7a3a739381
Dennis Hendriks <dh_tue@hotmail.com>
2024-06-02 15:38:01 +0200

    #823 Update CifToolPostCheckEnv for use by more tools.

--- cif/org.eclipse.escet.cif.typechecker/src/org/eclipse/escet/cif/typechecker/postchk/CifToolPostCheckEnv.java
+++ cif/org.eclipse.escet.cif.typechecker/src/org/eclipse/escet/cif/typechecker/postchk/CifToolPostCheckEnv.java
@@ -11,35 +11,42 @@
 // SPDX-License-Identifier: MIT
 //////////////////////////////////////////////////////////////////////////////
 
-package org.eclipse.escet.cif.merger;
+package org.eclipse.escet.cif.typechecker.postchk;
 
 import static org.eclipse.escet.common.java.Sets.set;
 import static org.eclipse.escet.common.java.Sets.sortedstrings;
+import static org.eclipse.escet.common.java.Strings.fmt;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.escet.cif.typechecker.ErrMsg;
-import org.eclipse.escet.cif.typechecker.postchk.CifPostCheckEnv;
 import org.eclipse.escet.common.app.framework.PlatformUriUtils;
-import org.eclipse.escet.common.app.framework.output.OutputProvider;
+import org.eclipse.escet.common.java.exceptions.UnsupportedException;
 import org.eclipse.escet.common.position.metamodel.position.Position;
 import org.eclipse.escet.common.typechecker.SemanticProblemSeverity;
 
-/** {@link CifPostCheckEnv} for the CIF merger. */
-public class CifMergerPostCheckEnv extends CifPostCheckEnv {
-    /** The absolute local file system path to the directory containing the merged CIF file. */
+/** {@link CifPostCheckEnv} for tools that are not the CIF type checker. */
+public class CifToolPostCheckEnv extends CifPostCheckEnv {
+    /** The absolute local file system path to the directory containing the CIF file to check. */
     private final String absCifDirPath;
+
+    /** A description of the CIF specification. For instance, {@code "merged"} for a 'merged CIF specification'. */
+    private final String specDescription;
 
     /** Error messages reported so far. */
     public Set<String> errors = set();
 
     /**
-     * Constructor for the {@link CifMergerPostCheckEnv} class.
+     * Constructor for the {@link CifToolPostCheckEnv} class.
      *
-     * @param absCifDirPath The absolute local file system path to the directory containing the merged CIF file.
+     * @param absCifDirPath The absolute local file system path to the directory containing the CIF file to check.
+     * @param specDescription A description of the CIF specification. For instance, {@code "merged"} for a 'merged CIF
+     *     specification'.
      */
-    public CifMergerPostCheckEnv(String absCifDirPath) {
+    public CifToolPostCheckEnv(String absCifDirPath, String specDescription) {
         this.absCifDirPath = absCifDirPath;
+        this.specDescription = specDescription;
     }
 
     @Override
@@ -60,18 +67,25 @@ public class CifMergerPostCheckEnv extends CifPostCheckEnv {
         return PlatformUriUtils.getRelativePath(absTargetPath, absCifDirPath);
     }
 
-    /** Prints the reported errors to the console, if any. */
-    public void printErrors() {
+    /**
+     * Throw an {@link UnsupportedException} if there are any errors, or do nothing otherwise.
+     *
+     * @param operationFailedMessage A message describing the operation that failed. It should be a full sentence,
+     *     starting with a capital letter, and ending with a period. It is used as message for a wrapping exception. If
+     *     {@code null}, no wrapping exception is created, and the detailed exception is thrown directly.
+     * @throws UnsupportedException If there are any errors.
+     */
+    public void throwUnsupportedExceptionIfAnyErrors(String operationFailedMessage) {
         if (errors.isEmpty()) {
             return;
         }
 
-        // Print header.
-        OutputProvider.err("Problems detected in merged CIF specification:");
-
-        // Print errors, sorted and without duplicates.
-        for (String error: sortedstrings(errors)) {
-            OutputProvider.err(" - ERROR: " + error);
+        String reasons = sortedstrings(errors).stream().map(m -> " - " + m).collect(Collectors.joining("\n"));
+        String message = fmt("The %s CIF specification is invalid:\n%s", specDescription, reasons);
+        if (operationFailedMessage == null) {
+            throw new UnsupportedException(message);
+        } else {
+            throw new UnsupportedException(operationFailedMessage, new UnsupportedException(message));
         }
     }
 }
