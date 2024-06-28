771c02ee2877f865cf415e875b45c26dd53ebce3
Albert Hofkamp <a.t.hofkamp@tue.nl>
2024-06-28 13:05:19 +0200

    #881 Adjust writing files in PLCgen ABB writer.

--- cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/targets/PlcBaseTarget.java
+++ cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/targets/PlcBaseTarget.java
@@ -355,6 +355,6 @@ public abstract class PlcBaseTarget extends PlcTarget {
     @Override
     public void writeOutput(PlcProject project) {
         Writer writer = getPlcCodeWriter();
-        writer.write(project, outputPaths.systemPath);
+        writer.write(project, outputPaths);
     }
 }
