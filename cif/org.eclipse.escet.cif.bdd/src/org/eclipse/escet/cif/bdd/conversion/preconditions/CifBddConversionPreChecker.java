5c59339cecfdd6b8617d304677d28751c6c7d298
Dennis Hendriks <dh_tue@hotmail.com>
2024-07-21 14:04:37 +0200

    #424 Add CifBddConversionPreChecker class. Empty for now.

---
+++ cif/org.eclipse.escet.cif.bdd/src/org/eclipse/escet/cif/bdd/conversion/preconditions/CifBddConversionPreChecker.java
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.bdd.conversion.preconditions;
+
+import java.util.function.BooleanSupplier;
+
+import org.eclipse.escet.cif.checkers.CifPreconditionChecker;
+
+/** CIF to BDD conversion precondition checker. */
+public class CifBddConversionPreChecker extends CifPreconditionChecker {
+    /**
+     * Constructor for the {@link CifBddConversionPreChecker} class.
+     *
+     * @param shouldTerminate Callback that indicates whether execution should be terminated on user request.
+     */
+    public CifBddConversionPreChecker(BooleanSupplier shouldTerminate) {
+        super(shouldTerminate,
+
+        );
+    }
+}
