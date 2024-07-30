b4208745d3ce8e3dd61e544514e35a62d80c2bce
Dennis Hendriks <dh_tue@hotmail.com>
2024-07-30 14:20:21 +0200

    #424 Add additional test for 'at most one' initial location.

---
+++ cif/org.eclipse.escet.cif.tests/src-test/org/eclipse/escet/cif/tests/checkers/checks/AutOnlyWithCertainNumberOfInitLocsCheckAtMostOne.java
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.tests.checkers.checks;
+
+import org.eclipse.escet.cif.checkers.checks.AutOnlyWithCertainNumberOfInitLocsCheck;
+
+/** {@link AutOnlyWithCertainNumberOfInitLocsCheck} that allows at most one initial location. */
+public class AutOnlyWithCertainNumberOfInitLocsCheckAtMostOne extends AutOnlyWithCertainNumberOfInitLocsCheck {
+    /** Constructor of the {@link AutOnlyWithCertainNumberOfInitLocsCheckAtMostOne} class. */
+    public AutOnlyWithCertainNumberOfInitLocsCheckAtMostOne() {
+        super(AllowedNumberOfInitLocs.AT_MOST_ONE);
+    }
+}
