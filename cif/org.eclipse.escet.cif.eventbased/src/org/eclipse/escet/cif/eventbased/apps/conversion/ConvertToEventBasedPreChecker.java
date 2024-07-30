d401e849a2cc2ab36966f0e312df49710f762e4d
Dennis Hendriks <dh_tue@hotmail.com>
2024-07-30 13:52:21 +0200

    #424 CIF event-based: add empty ConvertToEventBasedPreChecker class.

---
+++ cif/org.eclipse.escet.cif.eventbased/src/org/eclipse/escet/cif/eventbased/apps/conversion/ConvertToEventBasedPreChecker.java
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010, 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.eventbased.apps.conversion;
+
+import org.eclipse.escet.cif.checkers.CifPreconditionChecker;
+import org.eclipse.escet.common.java.Termination;
+
+/** CIF to event-based conversion precondition checker. */
+public class ConvertToEventBasedPreChecker extends CifPreconditionChecker {
+    /**
+     * Constructor for the {@link ConvertToEventBasedPreChecker} class.
+     *
+     * @param termination Cooperative termination query function.
+     */
+    public ConvertToEventBasedPreChecker(Termination termination) {
+        super(termination
+
+        );
+    }
+}
