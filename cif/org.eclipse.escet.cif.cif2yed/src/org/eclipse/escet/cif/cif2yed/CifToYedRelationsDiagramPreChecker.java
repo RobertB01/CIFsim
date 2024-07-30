611f7938688116979e6774a2103ab13ea02e9ceb
Dennis Hendriks <dh_tue@hotmail.com>
2024-07-30 13:16:23 +0200

    #424 CIF to yEd: add empty CifToYedPreChecker class.

---
+++ cif/org.eclipse.escet.cif.cif2yed/src/org/eclipse/escet/cif/cif2yed/CifToYedRelationsDiagramPreChecker.java
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010, 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.cif2yed;
+
+import org.eclipse.escet.cif.checkers.CifPreconditionChecker;
+import org.eclipse.escet.common.java.Termination;
+
+/** CIF to yEd transformation precondition checker for generating relations diagrams. */
+public class CifToYedRelationsDiagramPreChecker extends CifPreconditionChecker {
+    /**
+     * Constructor for the {@link CifToYedRelationsDiagramPreChecker} class.
+     *
+     * @param termination Cooperative termination query function.
+     */
+    public CifToYedRelationsDiagramPreChecker(Termination termination) {
+        super(termination
+
+        );
+    }
+}
