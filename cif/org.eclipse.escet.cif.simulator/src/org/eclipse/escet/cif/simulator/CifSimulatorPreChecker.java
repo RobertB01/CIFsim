d2dac50a5ca7e55a132023ab97270d64859f92a0
Dennis Hendriks <dh_tue@hotmail.com>
2024-07-30 11:34:56 +0200

    #424 CIF simulator: add empty CifSimulatorPreChecker class.

---
+++ cif/org.eclipse.escet.cif.simulator/src/org/eclipse/escet/cif/simulator/CifSimulatorPreChecker.java
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010, 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package org.eclipse.escet.cif.simulator;
+
+import org.eclipse.escet.cif.checkers.CifPreconditionChecker;
+import org.eclipse.escet.common.java.Termination;
+
+/** CIF simulator precondition checker. */
+public class CifSimulatorPreChecker extends CifPreconditionChecker {
+    /**
+     * Constructor for the {@link CifSimulatorPreChecker} class.
+     *
+     * @param termination Cooperative termination query function.
+     */
+    public CifSimulatorPreChecker(Termination termination) {
+        super(termination
+
+        );
+    }
+}
