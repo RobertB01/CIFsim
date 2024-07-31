b39ca8af93bc64c0842bc37cbe8f48a6a498ea85
Albert Hofkamp <a.t.hofkamp@tue.nl>
2024-07-31 13:01:29 +0200

    #945 Rework the NameGenerator interface,
    - Add a set prefixes that can be safely prepended to a generated name.
    - Replace the Map with base names + numbers by the NameScope class.

--- cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/generators/NameGenerator.java
+++ cif/org.eclipse.escet.cif.plcgen/src/org/eclipse/escet/cif/plcgen/generators/NameGenerator.java
@@ -13,43 +13,90 @@
 
 package org.eclipse.escet.cif.plcgen.generators;
 
-import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.escet.cif.plcgen.generators.names.NameScope;
 import org.eclipse.escet.common.position.metamodel.position.PositionObject;
 
 /** Code generator interface for a {@link DefaultNameGenerator}. */
 public interface NameGenerator {
     /**
-     * Convert the given object to a proper name that does not clash with the PLC language or with previously generated
-     * global names. This function should not be used for generating names that also may contain local generated names.
+     * Convert the given object to a valid name in the global scope that does not clash with the PLC language or with
+     * previously generated names. The returned name should be used as-is.
      *
      * @param posObject Named CIF object.
-     * @return A proper name that does not clash with PLC language keywords or previously generated global names.
+     * @return A valid name that does not clash with PLC language keywords or previously generated names. The returned
+     *     name should be used as-is.
+     * @see #generateGlobalNames(Set, PositionObject)
      */
     public abstract String generateGlobalName(PositionObject posObject);
 
     /**
-     * Convert the given name to a proper name that does not clash with the PLC language or with previously generated
-     * global names. This function should not be used for generating names that also may contain local generated names.
+     * Convert the given object to a valid suffix name in the global scope that does not clash with the PLC language or
+     * with previously generated names. Only the given prefixes should be prepended to the returned name before use.
      *
-     * @param initialName Suggested name to to use.
-     * @param initialIsCifName Whether the initial name is known by the CIF user. Used to produce rename warnings. As
-     *     producing such rename warnings for elements that have no name in CIF is meaningless to a user, this parameter
-     *     should be {@code false} for those names.
-     * @return A proper name that does not clash with PLC language keywords or previously generated global names.
+     * @param prefixes Set of identifier prefixes, preferably with a trailing underscore. One of the provided prefixes
+     *     must be prepended to the returned name to obtain a valid name to use in the PLC code.
+     * @param posObject The named CIF object to be converted to a name.
+     * @return A valid name that does not clash with PLC language keywords or previously generated names. The returned
+     *     name should be prepended with one of the given prefixes before use.
+     * @see #generateGlobalName(PositionObject)
      */
-    public abstract String generateGlobalName(String initialName, boolean initialIsCifName);
+    public abstract String generateGlobalNames(Set<String> prefixes, PositionObject posObject);
 
     /**
-     * Convert the given name to a proper name that does not clash with the PLC language, with previously generated
-     * global names or with previously generated local names that used the same {@code localSuffixes} information.
+     * Convert the given name to a valid name in the global scope that does not clash with the PLC language or with
+     * previously generated names. The returned name should be used as-is.
      *
-     * @param initialName Suggested name to to use.
-     * @param localSuffixes Name suffix information of local names. Use the same map to generate all local names in a
-     *     scope.
-     * @return A proper name that does not clash with PLC language keywords, previously generated global names or local
-     *     names created using this function with the same {@code localSuffixes} map.
-     * @note Local names are assumed not to be used for representing CIF variables.
+     * @param initialName Suggested name to use. The method may return a different name.
+     * @param isCifName Whether the initial name is known by the CIF user. Used to produce rename warnings. As producing
+     *     such rename warnings for elements that have no name in CIF is meaningless to a user, this parameter should be
+     *     {@code false} for those names.
+     * @return A valid name that does not clash with PLC language keywords or previously generated names. The returned
+     *     name should be used as-is.
+     * @see #generateGlobalNames(Set, String, boolean)
      */
-    public abstract String generateLocalName(String initialName, Map<String, Integer> localSuffixes);
+    public abstract String generateGlobalName(String initialName, boolean isCifName);
+
+    /**
+     * Convert the given object to a valid suffix name in the global scope that does not clash with the PLC language or
+     * with previously generated names. Only the given prefixes should be prepended to the returned name before use.
+     *
+     * @param prefixes Set of identifier prefixes, preferably with a trailing underscore. One of the provided prefixes
+     *     must be prepended to the returned name to obtain a valid name to use in the PLC code.
+     * @param initialName Suggested name to use. The method may return a different name.
+     * @param isCifName Whether the initial name is known by the CIF user. Used to produce rename warnings. As producing
+     *     such rename warnings for elements that have no name in CIF is meaningless to a user, this parameter should be
+     *     {@code false} for those names.
+     * @return A valid name that does not clash with PLC language keywords or previously generated names. The returned
+     *     name should be prepended with one of the given prefixes before use.
+     * @see #generateGlobalName(String, boolean)
+     */
+    public abstract String generateGlobalNames(Set<String> prefixes, String initialName, boolean isCifName);
+
+    /**
+     * Convert the given name to a valid name in the provided scope that does not clash with the PLC language or with
+     * previously generated names. The returned name should be used as-is.
+     *
+     * @param initialName Suggested name to use. The method may return a different name.
+     * @param localScope Scope that will use the generated name.
+     * @return A valid name that does not clash with PLC language keywords or previously generated names. The returned
+     *     name should be used as-is.
+     * @see #generateLocalNames(Set, String, NameScope)
+     */
+    public abstract String generateLocalName(String initialName, NameScope localScope);
+
+    /**
+     * Convert the given object to a valid suffix name in the provided scope that does not clash with the PLC language
+     * or with previously generated names. Only the given prefixes should be prepended to the returned name before use.
+     *
+     * @param prefixes Set of identifier prefixes, preferably with a trailing underscore. One of the provided prefixes
+     *     must be prepended to the returned name to obtain a valid name to use in the PLC code.
+     * @param initialName Suggested name to use. The method may return a different name.
+     * @param localScope Scope that will use the generated names.
+     * @return A valid name that does not clash with PLC language keywords or previously generated names. The returned
+     *     name should be prepended with one of the given prefixes before use.
+     * @see #generateLocalName(String, NameScope)
+     */
+    public abstract String generateLocalNames(Set<String> prefixes, String initialName, NameScope localScope);
 }
