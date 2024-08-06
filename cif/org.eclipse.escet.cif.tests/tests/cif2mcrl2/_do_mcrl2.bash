838f40231e8cf19956b607ee1532d68a329c6a7d
Dennis Hendriks <dh_tue@hotmail.com>
2024-08-06 12:39:03 +0200

    #352 Add missing license header. Make script executable.

--- cif/org.eclipse.escet.cif.tests/tests/cif2mcrl2/_do_mcrl2.bash
+++ cif/org.eclipse.escet.cif.tests/tests/cif2mcrl2/_do_mcrl2.bash
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+################################################################################
+# Copyright (c) 2024 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the terms
+# of the MIT License which is available at https://opensource.org/licenses/MIT
+#
+# SPDX-License-Identifier: MIT
+################################################################################
+
 for f in `find -name "*.mcrl2" -type f`
 do
     echo $f
